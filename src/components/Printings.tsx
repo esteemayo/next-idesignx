0c95b8281d4fe9df6522bda27aae8eba74c7d347
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 01:48:50 +0100

    feat: create Printings component

---
+++ src/components/Printings.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Printings = () => {
+  return <Container>Printings</Container>;
+};
+
+const Container = styled.section`
+  background-color: red;
+`;
+
+export default Printings;
