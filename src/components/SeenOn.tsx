85b91dc111a1b4b13a8020530ac1e7c3211ce829
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 01:49:10 +0100

    feat: create SeenOn component

---
+++ src/components/SeenOn.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const SeenOn = () => {
+  return <Container>SeenOn</Container>;
+};
+
+const Container = styled.section`
+  background-color: greenyellow;
+`;
+
+export default SeenOn;
