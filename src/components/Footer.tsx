e8dde948e329cb1cb9812e8517228e66c0c9961f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 13:12:01 +0100

    fix: add styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,15 @@ const Footer = () => {
 };
 
 const Container = styled.footer`
-  background-color: darkslategray;
+  padding: 8rem;
+  background-image: linear-gradient(
+    to right bottom,
+    var(--clr-purple-dark),
+    var(--clr-purple-light-1),
+    var(--clr-purple-light-2),
+    var(--clr-purple-light-3)
+  );
+  color: var(--clr-white);
 `;
 
 export default Footer;
