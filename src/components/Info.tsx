d0e34b64f377122304c3ea65c7bb9ab0d0972208
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 01:32:41 +0100

    feat: create Info component

---
+++ src/components/Info.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Info = () => {
+  return <Container>Info</Container>;
+};
+
+const Container = styled.div`
+  background-color: palevioletred;
+`;
+
+export default Info;
