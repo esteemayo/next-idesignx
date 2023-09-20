45a6b3cfda1c76d2ab05e895ad8b0b4a2cc623de
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 01:48:40 +0100

    feat: create Hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Hero = () => {
+  return <Container>Header</Container>;
+};
+
+const Container = styled.header`
+  background-color: yellow;
+`;
+
+export default Hero;
