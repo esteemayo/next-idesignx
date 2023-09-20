5a5ebde32f0c4812ad7dc794d98bed63d54c8638
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 01:31:28 +0100

    feat: create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Footer = () => {
+  return <Container>Footer</Container>;
+};
+
+const Container = styled.footer`
+  background-color: darkslategray;
+`;
+
+export default Footer;
