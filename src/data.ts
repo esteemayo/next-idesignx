5cd43a046e311de7e49e62090ead5133662fb593
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 13:23:00 +0100

    fix: add export const menuItems = [

--- src/data.ts
+++ src/data.ts
@@ -30,3 +30,31 @@ export const navItems = [
     label: 'Contact',
   },
 ];
+
+export const menuItems = [
+  {
+    id: 1,
+    url: '/about',
+    label: 'About',
+  },
+  {
+    id: 2,
+    url: '/products',
+    label: 'Products',
+  },
+  {
+    id: 3,
+    url: '/gallery',
+    label: 'Gallery',
+  },
+  {
+    id: 4,
+    url: '/help',
+    label: 'Help',
+  },
+  {
+    id: 5,
+    url: '/contact',
+    label: 'Contact',
+  },
+];
