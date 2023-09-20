b4455d3d76dc5ee3594af5881cf3e58e5a2988e4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 13:16:44 +0100

    feat: create MenuLink component

---
+++ src/components/footer/MenuLink.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const MenuLink = () => {
+  return <Container>MenuLink</Container>;
+};
+
+const Container = styled.li``;
+
+export default MenuLink;
