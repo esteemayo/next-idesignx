107b6bae3fc57b00b0bbdc8a936a2de1804f907e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 12:22:32 +0100

    fix: refactor

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -26,26 +26,6 @@ const Container = styled.nav`
   align-items: center;
 `;
 
-const NavItem = styled.li`
-  a {
-    &:link,
-    &:visited {
-      text-decoration: none;
-      text-transform: capitalize;
-      font-weight: 300;
-      font-size: 1.6rem;
-      color: var(--clr-purple-light-3);
-      outline-color: #eee;
-      transition: all 0.2s;
-    }
-
-    &:hover,
-    &:active {
-      color: var(--clr-purple-light-2);
-    }
-  }
-`;
-
 const Button = styled.button`
   justify-self: end;
 
