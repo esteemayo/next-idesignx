1d34b14f88189e162908d56e9eba64f22d0b3b48
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 12:26:44 +0100

    fix: add navItems data array

---
+++ src/data.ts
@@ -0,0 +1,32 @@
+export const navItems = [
+  {
+    id: 1,
+    url: '/',
+    label: 'Home',
+  },
+  {
+    id: 2,
+    url: '/designs',
+    label: 'Designs',
+  },
+  {
+    id: 3,
+    url: '/prints',
+    label: 'Prints',
+  },
+  {
+    id: 4,
+    url: '/gallery',
+    label: 'Gallery',
+  },
+  {
+    id: 5,
+    url: '/about',
+    label: 'About',
+  },
+  {
+    id: 6,
+    url: '/contact',
+    label: 'Contact',
+  },
+];
