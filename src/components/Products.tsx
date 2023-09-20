9a202ca345d8f82b7809b4def2e9b5558b9e9379
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 01:49:00 +0100

    feat: create Products component

---
+++ src/components/Products.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Products = () => {
+  return <Container>Products</Container>;
+};
+
+const Container = styled.section`
+  background-color: blue;
+`;
+
+export default Products;
