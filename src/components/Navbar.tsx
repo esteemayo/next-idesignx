7f54afcccc9f59fa76c1f1608ae27f3cd0816ecd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 04:14:09 +0100

    fix: add styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,7 +48,9 @@ const LogoBox = styled.div`
   justify-self: start;
 `;
 
-const StyledLink = styled(Link)``;
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
 
 const NavItems = styled.ul`
   list-style: none;
