bc569b63a649bdaee830c86784c864ebb43a36d5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 13:49:52 +0100

    fix: add static type checking

--- src/components/footer/MenuLink.tsx
+++ src/components/footer/MenuLink.tsx
@@ -1,11 +1,23 @@
 'use client';
 
+import Link from 'next/link';
 import styled from 'styled-components';
 
-const MenuLink = () => {
-  return <Container>MenuLink</Container>;
+interface MenuLinkProps {
+  url: string;
+  label: string;
+}
+
+const MenuLink: React.FC<MenuLinkProps> = ({ url, label }) => {
+  return (
+    <Container>
+      <StyledLink href={url}>{label}</StyledLink>
+    </Container>
+  );
 };
 
 const Container = styled.li``;
+
+const StyledLink = styled(Link)``;
 
 export default MenuLink;
