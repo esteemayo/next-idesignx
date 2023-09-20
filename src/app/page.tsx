fe591068046c24aa43934058c0c333b5e406e79a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 01:56:58 +0100

    fix: add markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,35 @@
 
 import styled from 'styled-components';
 
+import About from '@/components/About';
+import Contact from '@/components/Contact';
+import Hero from '@/components/Hero';
+import Printings from '@/components/Printings';
+import Products from '@/components/Products';
+import SeenOn from '@/components/SeenOn';
+
 const Home = () => {
   return (
-    <main>
-      <h1>Home page</h1>
-    </main>
+    <Container>
+      <Hero />
+      <About />
+      <Contact />
+      <SeenOn />
+      <Products />
+      <Printings />
+    </Container>
   );
 };
 
 const Container = styled.div`
   display: grid;
   grid-template-rows: 80vh repeat(4, min-content);
+  grid-template-columns:
+    [full-start] minmax(6rem, 1fr) [center-start] repeat(
+      8,
+      [col-start] minmax(min-content, 15rem) [col-end]
+    )
+    [center-end] minmax(6rem, 1fr) [full-end];
 `;
 
 export default Home;
