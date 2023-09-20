c242c631e4b4c92515c7889432555ea8e419c349
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 03:29:01 +0100

    fix: add markup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,25 @@
 'use client';
 
+import Link from 'next/link';
 import styled from 'styled-components';
 
 const Navbar = () => {
-  return <Container>Navbar</Container>;
+  return (
+    <Container>
+      <LogoBox>
+        <Logo href='/'>Logo</Logo>
+      </LogoBox>
+    </Container>
+  );
 };
 
 const Container = styled.nav`
   background-color: var(--clr-white);
   padding: 2.5rem;
 `;
+
+const LogoBox = styled.div``;
+
+const Logo = styled(Link)``;
 
 export default Navbar;
