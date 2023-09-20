c59be026d5c69d1b8298dcb880a92420048f9c8f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 01:30:34 +0100

    feat: create Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Navbar = () => {
+  return <Container>Navbar</Container>;
+};
+
+const Container = styled.nav`
+  background-color: orangered;
+`;
+
+export default Navbar;
