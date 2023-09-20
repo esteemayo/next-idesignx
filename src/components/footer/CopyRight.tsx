8bb6e28428a1e290acfbf636130a7c580affd466
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 14:04:22 +0100

    feat: create CopyRight component

---
+++ src/components/footer/CopyRight.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import styled from 'styled-components';
+
+interface CopyRightProps {
+  value: string;
+}
+
+const CopyRight: React.FC<CopyRightProps> = ({ value }) => {
+  return (
+    <Container>
+      &copy; Copyright <Year>{value}</Year> by Emmanuel Adebayo. All rights
+      reserved. We are a young company always looking for new and creative ideas
+      to help you with our products in your everyday work.
+    </Container>
+  );
+};
+
+const Container = styled.p``;
+
+const Year = styled.span``;
+
+export default CopyRight;
