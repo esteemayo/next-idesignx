71e0746c2aa2e6adb4094e0e7da6419289d24a08
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 16:17:27 +0100

    fix: add styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,6 +30,8 @@ const Header = styled.header`
   display: grid;
   grid-template-rows: repeat(3, 1fr);
   grid-template-columns: repeat(4, 1fr);
+  align-items: center;
+  justify-items: center;
 `;
 
 const Heading = styled.h1``;
