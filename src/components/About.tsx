feaf49560ec30d53dd1ef604dd57754edda820f3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-20 01:48:24 +0100

    feat: create About component

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const About = () => {
+  return <Container>About</Container>;
+};
+
+const Container = styled.section`
+  background-color: darkslateblue;
+`;
+
+export default About;
