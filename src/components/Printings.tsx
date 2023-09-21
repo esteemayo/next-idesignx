d6dd0b7b13bacdc0bf1ada1a3d9706412ec242af
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 16:55:35 +0100

    fix: add styling

--- src/components/Printings.tsx
+++ src/components/Printings.tsx
@@ -21,8 +21,10 @@ const Printings = () => {
 
 const Container = styled.section`
   grid-column: full-start / full-end;
+  padding: 7rem;
   background-image: linear-gradient(rgba(9, 9, 9, 0.5), rgba(9, 9, 9, 0.5)),
     url(../img/woman-2.jpg);
+  color: var(--clr-white);
   background-size: cover;
   background-position: center;
 `;
