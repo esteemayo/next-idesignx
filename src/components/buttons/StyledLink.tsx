2c269fe05f121e11add7fe46397975e975c74e12
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 15:55:35 +0100

    fix: add StyledLink component

---
+++ src/components/buttons/StyledLink.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+export const StyledLink = styled(Link)`
+  display: inline-block;
+  border: none;
+  text-decoration: none;
+  text-transform: capitalize;
+  padding: 1rem 3rem;
+  font-weight: 400;
+  font-size: 1.5rem;
+  background-image: linear-gradient(
+    to right bottom,
+    var(--clr-purple-dark),
+    var(--clr-purple-light-1),
+    var(--clr-purple-light-2),
+    var(--clr-purple-light-3)
+  );
+  color: var(--clr-white);
+  border-radius: 0.5rem;
+  outline-color: var(--clr-purple-light-3);
+  transition: all 0.2s;
+
+  &:active {
+    color: currentColor;
+  }
+
+  &:hover {
+    opacity: 0.9;
+  }
+`;
