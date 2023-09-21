5a98a63624cc64398d22222fe2f0461fd0011cee
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 14:25:55 +0100

    fix: refactor

--- src/components/inputs/TextArea.tsx
+++ src/components/inputs/TextArea.tsx
@@ -34,15 +34,6 @@ const TextArea: React.FC<TextAreaProps> = ({
   );
 };
 
-// const Label = styled.label`
-//   display: block;
-//   font-weight: 400;
-//   font-size: 1.4rem;
-//   color: var(--clr-gray-dark-2);
-//   margin-left: 2rem;
-//   margin-top: 0.7rem;
-// `;
-
 // const StyledTextArea = styled.textarea`
 //   width: 100%;
 //   display: inline-block;
