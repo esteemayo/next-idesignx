0fbc4b3c7ab280245c9eaef1618da90493da2810
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 14:11:28 +0100

    fix: add styling

--- src/components/inputs/Select.tsx
+++ src/components/inputs/Select.tsx
@@ -47,10 +47,12 @@ const StyledSelect = styled.select`
   padding: 1.5rem 2rem;
   background-color: var(--clr-white);
   color: var(--clr-gray-dark-2);
+  border: 3px solid transparent;
   border-radius: 2px;
-  outline-color: var(--clr-purple-light-3);
 
   &:focus {
+    outline: none;
+    border-bottom: 3px solid var(--clr-green);
     box-shadow: 0 0 1rem rgba(0, 0, 0, 0.07);
   }
 `;
