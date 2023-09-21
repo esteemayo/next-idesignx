fa76adb59f57181ce1bc434c28445586b6235366
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 16:57:15 +0100

    fix: add styling

--- src/components/Printings.tsx
+++ src/components/Printings.tsx
@@ -29,7 +29,11 @@ const Container = styled.section`
   background-position: center;
 `;
 
-const Heading = styled.h4``;
+const Heading = styled.h4`
+  font-weight: 400;
+  font-size: 1.9rem;
+  text-transform: capitalize;
+`;
 
 const Wrapper = styled.div``;
 
