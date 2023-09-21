ebd550ef115554dfa78b3d2e0a59e972b05e1c64
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 15:56:57 +0100

    fix: refactor

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -49,33 +49,4 @@ const Text = styled.p`
   margin-bottom: 6rem;
 `;
 
-// const StyledLink = styled(Link)`
-//   display: inline-block;
-//   border: none;
-//   text-decoration: none;
-//   text-transform: capitalize;
-//   padding: 1rem 3rem;
-//   font-weight: 400;
-//   font-size: 1.5rem;
-//   background-image: linear-gradient(
-//     to right bottom,
-//     var(--clr-purple-dark),
-//     var(--clr-purple-light-1),
-//     var(--clr-purple-light-2),
-//     var(--clr-purple-light-3)
-//   );
-//   color: var(--clr-white);
-//   border-radius: 0.5rem;
-//   outline-color: var(--clr-purple-light-3);
-//   transition: all 0.2s;
-
-//   &:active {
-//     color: currentColor;
-//   }
-
-//   &:hover {
-//     opacity: 0.9;
-//   }
-// `;
-
 export default About;
