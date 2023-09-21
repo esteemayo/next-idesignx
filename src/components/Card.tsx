b424f4a4b390d6f537018bb1e9b0b1e15f147d09
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 16:22:50 +0100

    feat: create Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Card = () => {
+  return <Container>Card</Container>;
+};
+
+const Container = styled.article``;
+
+export default Card;
