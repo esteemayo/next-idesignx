8d100ff142f8d9b677426d5de522dd64965c992a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 14:17:42 +0100

    fix: create Label component

---
+++ src/components/form/Label.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  display: block;
+  font-weight: 400;
+  font-size: 1.4rem;
+  color: var(--clr-gray-dark-2);
+  margin-left: 2rem;
+  margin-top: 0.7rem;
+`;
