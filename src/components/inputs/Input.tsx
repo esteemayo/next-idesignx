530eb9164056df782f7e8dd052a61d1617acf42a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 14:21:15 +0100

    fix: refactor

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -2,6 +2,10 @@
 
 import styled from 'styled-components';
 
+import { StyledInput } from '../form/StyleInput';
+import { Label } from '../form/Label';
+import { FormGroup } from '../form/FormGroup';
+
 interface InputProps {
   name: string;
   label: string;
@@ -35,56 +39,56 @@ const Input: React.FC<InputProps> = ({
   );
 };
 
-const FormGroup = styled.div`
-  &:not(:last-of-type) {
-    margin-bottom: 2rem;
-  }
-`;
+// const FormGroup = styled.div`
+//   &:not(:last-of-type) {
+//     margin-bottom: 2rem;
+//   }
+// `;
 
-const Label = styled.label`
-  display: block;
-  font-weight: 400;
-  font-size: 1.4rem;
-  color: var(--clr-gray-dark-2);
-  margin-left: 2rem;
-  margin-top: 0.7rem;
-`;
+// const Label = styled.label`
+//   display: block;
+//   font-weight: 400;
+//   font-size: 1.4rem;
+//   color: var(--clr-gray-dark-2);
+//   margin-left: 2rem;
+//   margin-top: 0.7rem;
+// `;
 
-const StyledInput = styled.input`
-  width: 100%;
-  display: inline-block;
-  border: none;
-  font-family: inherit;
-  font-size: 1.5rem;
-  padding: 1.5rem 2rem;
-  background-color: var(--clr-white);
-  color: var(--clr-gray-dark-2);
-  border: 3px solid transparent;
-  border-radius: 2px;
-  outline-color: var(--clr-purple-light-3);
-  caret-color: var(--clr-purple-light-3);
-  transition: all 0.3s;
+// const StyledInput = styled.input`
+//   width: 100%;
+//   display: inline-block;
+//   border: none;
+//   font-family: inherit;
+//   font-size: 1.5rem;
+//   padding: 1.5rem 2rem;
+//   background-color: var(--clr-white);
+//   color: var(--clr-gray-dark-2);
+//   border: 3px solid transparent;
+//   border-radius: 2px;
+//   outline-color: var(--clr-purple-light-3);
+//   caret-color: var(--clr-purple-light-3);
+//   transition: all 0.3s;
 
-  &::placeholder {
-    font-size: 1.4rem;
-    color: var(--clr-gray-dark-2);
-  }
+//   &::placeholder {
+//     font-size: 1.4rem;
+//     color: var(--clr-gray-dark-2);
+//   }
 
-  &:focus {
-    outline: none;
-    border-bottom: 3px solid var(--clr-green);
-    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.07);
-  }
+//   &:focus {
+//     outline: none;
+//     border-bottom: 3px solid var(--clr-green);
+//     box-shadow: 0 0 1rem rgba(0, 0, 0, 0.07);
+//   }
 
-  &:focus:invalid {
-    border-bottom: 3px solid var(--clr-orange-dark);
-  }
+//   &:focus:invalid {
+//     border-bottom: 3px solid var(--clr-orange-dark);
+//   }
 
-  &:placeholder-shown + ${Label} {
-    opacity: 0;
-    visibility: hidden;
-    transform: translateY(-4rem);
-  }
-`;
+//   &:placeholder-shown + ${Label} {
+//     opacity: 0;
+//     visibility: hidden;
+//     transform: translateY(-4rem);
+//   }
+// `;
 
 export default Input;
