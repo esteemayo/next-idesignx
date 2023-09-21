7d02a288b5f2ef43e4118176862c0cbf2c69805a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 14:55:31 +0100

    fix: add img element tag

--- src/components/SeenOn.tsx
+++ src/components/SeenOn.tsx
@@ -7,7 +7,38 @@ const SeenOn = () => {
   return (
     <Container>
       <Text>As seen on</Text>
-      <LogoContainer></LogoContainer>
+      <LogoContainer>
+        <StyledImage
+          src='/img/access.jpg'
+          width={60}
+          height={50}
+          alt=''
+        />
+        <StyledImage
+          src='/img/Stanbic-Bank-Logo.jpg'
+          width={60}
+          height={50}
+          alt=''
+        />
+        <StyledImage
+          src='/img/zenith.png'
+          width={60}
+          height={50}
+          alt=''
+        />
+        <StyledImage
+          src='/img/union-bank.jpg'
+          width={60}
+          height={50}
+          alt=''
+        />
+        <StyledImage
+          src='/img/first-bank.png'
+          width={60}
+          height={50}
+          alt=''
+        />
+      </LogoContainer>
     </Container>
   );
 };
