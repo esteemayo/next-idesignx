1f55da00684f16eb696cfe505dc4594c0d7379f1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-21 15:58:58 +0100

    fix: add anchor tag

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 import { productImages } from '@/data';
 import ProductImage from './ProductImage';
+import { StyledLink } from './buttons/StyledLink';
 
 const Products = () => {
   return (
@@ -14,6 +15,7 @@ const Products = () => {
           return <ProductImage key={item.id} {...item} />;
         })}
       </Wrapper>
+      <StyledLink href='/products'>All products</StyledLink>
     </Container>
   );
 };
