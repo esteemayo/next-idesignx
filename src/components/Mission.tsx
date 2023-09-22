dceb1f009486cbff6e9aa41179e26fb894e2fdc6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 03:33:13 +0100

    fix: add Mission component

---
+++ src/components/Mission.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Mission = () => {
+  return <Container>Mission</Container>;
+};
+
+const Container = styled.section`
+  background-color: orangered;
+`;
+
+export default Mission;
