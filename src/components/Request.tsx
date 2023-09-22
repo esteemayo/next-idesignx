4eeb0f496490b9c698ef263604b948c331c3ed16
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 03:38:23 +0100

    fix: add Request component

---
+++ src/components/Request.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Request = () => {
+  return <Container>Request</Container>;
+};
+
+const Container = styled.section`
+  background-color: palevioletred;
+`;
+
+export default Request;
