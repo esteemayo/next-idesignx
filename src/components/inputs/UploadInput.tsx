9ee785bbf742a6e461b546556f7acbd6af7092b6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 16:26:04 +0100

    fix: add UploadInput component

---
+++ src/components/inputs/UploadInput.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const UploadInput = () => {
+  return <Container>UploadInput</Container>;
+};
+
+const Container = styled.div``;
+
+export default UploadInput;
