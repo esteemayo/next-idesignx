ffb900ef3f60dc6222f2314c31466950f8ee7f43
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 14:38:32 +0100

    fix: add img element tag

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import styled from 'styled-components';
 
 const Featured = () => {
@@ -10,5 +11,7 @@ const Container = styled.section`
   grid-column: full-start / col-end 4;
   background-color: var(--clr-white);
 `;
+
+const StyledImage = styled(Image)``;
 
 export default Featured;
