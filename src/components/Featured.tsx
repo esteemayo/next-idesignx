938b04876d0b148791dca5b73c6500d0747f7dd6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 03:32:09 +0100

    fix: add Featured component

---
+++ src/components/Featured.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Featured = () => {
+  return <Container>Featured</Container>;
+};
+
+const Container = styled.section`
+  background-color: crimson;
+`;
+
+export default Featured;
