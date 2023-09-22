46752a5dee4fd3529732078f93a77fbaee4a9dff
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 03:21:06 +0100

    fix: create DesignHero component

---
+++ src/components/hero/DesignHero.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const DesignHero = () => {
+  return <Header>Custom designs</div>;
+};
+
+const Header = styled.header``;
+
+export default DesignHero;
