fa4aa54461568f4714a999aaec65e192654c20e0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 15:53:24 +0100

    fix: add handleChange function

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import styled from 'styled-components';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 import Form from './form/Form';
 import Input from './inputs/Input';
 
 const Request = () => {
+  const [data, setData] = useState({});
+
+  const handleChange = useCallback(({ target: input }) => {
+    const { name, value } = input;
+    setData((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
   const handleSubmit = useCallback((e) => {
     e.preventDefault();
   }, []);
