bfd10acd331eca9cc95b465858105afb925f72ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 01:35:59 +0100

    fix: add icon

--- src/components/buttons/ScrollButton.tsx
+++ src/components/buttons/ScrollButton.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 const ScrollButton = () => {
   return (
     <Container>
-      <Button>^</Button>
+      <Button>
+        <FontAwesomeIcon icon={faArrowUp} />
+      </Button>
     </Container>
   );
 };
