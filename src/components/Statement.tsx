50239ce828d2617469412fc3c0f274186de23d05
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 03:42:00 +0100

    fix: add Statement component

---
+++ src/components/Statement.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Statement = () => {
+  return <Container>Statement</Container>;
+};
+
+const Container = styled.section`
+  background-color: darkslateblue;
+`;
+
+export default Statement;
