2219442b5148e7a7f63ae5cd4e4fd716de37d9c8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 00:47:03 +0100

    feat: create Hamburger component

---
+++ src/components/navbar/Hamburger.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Hamburger = () => {
+  return <Container>Hamburger</Container>;
+};
+
+const Container = styled.button``;
+
+export default Hamburger;
