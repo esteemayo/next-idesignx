505090a656d632f2086f4813d0b75bc24d8cc47f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 16:04:43 +0100

    fix: refactor

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -91,34 +91,4 @@ const Text = styled.p`
   color: var(--clr-purple-light-1);
 `;
 
-// const Button = styled.button`
-//   margin-top: 2rem;
-//   display: inline-block;
-//   border: none;
-//   text-transform: capitalize;
-//   padding: 1rem 3rem;
-//   font-weight: 400;
-//   font-size: 1.5rem;
-//   background-image: linear-gradient(
-//     to right bottom,
-//     var(--clr-purple-dark-1),
-//     var(--clr-purple-light-1),
-//     var(--clr-purple-light-2),
-//     var(--clr-purple-light-3)
-//   );
-//   color: var(--clr-white);
-//   border-radius: 0.5rem;
-//   outline-color: var(--clr-purple-light-3);
-//   cursor: pointer;
-//   transition: all 0.2s;
-
-//   &:hover {
-//     opacity: 0.9;
-//   }
-
-//   &:disabled {
-//     opacity: 0.75;
-//   }
-// `;
-
 export default Contact;
