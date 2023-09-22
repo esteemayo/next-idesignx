ca6ab7025afa0ad7fb0ec5ba68aab4596ef53331
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 03:16:13 +0100

    fix: add styling

--- src/app/designs/page.tsx
+++ src/app/designs/page.tsx
@@ -6,6 +6,15 @@ const Designs = () => {
   return <Container>Designs</Container>;
 };
 
-const Container = styled.main``;
+const Container = styled.main`
+  display: grid;
+  grid-template-rows: 80vh 40vw repeat(2, min-content);
+  grid-template-columns:
+    [full-start] minmax(6rem, 1fr) [center-start] repeat(
+      8,
+      [col-start] minmax(min-content, 15rem) [col-end]
+    )
+    [center-end] minmax(6rem, 1fr) [full-end];
+`;
 
 export default Designs;
