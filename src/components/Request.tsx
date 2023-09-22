ad3025fd22cfa1cc7dbbd3b990a431d19ce10b4f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-22 15:47:45 +0100

    fix: add Form component

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import styled from 'styled-components';
+import { useCallback } from 'react';
+
+import Form from './form/Form';
 
 const Request = () => {
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+  }, []);
+
   return (
     <Container>
       <Heading>Fill in your requests</Heading>
+      <Form onSubmit={handleSubmit}></Form>
     </Container>
   );
 };
