73db1103aa379268f551e1a643c2e0d680fc6ad4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 21:33:09 +0100

    fix: create PrintContact component

---
+++ src/components/PrintContact.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import styled from 'styled-components';
+
+const PrintContact = () => {
+  return <Container>PrintContact</Container>;
+};
+
+const Container = styled.section`
+  grid-column: full-start / full-end;
+  background-image: linear-gradient(
+      to right bottom,
+      rgba(0, 0, 0, 0.7),
+      rgba(51, 51, 51, 0.7)
+    ),
+    url(/img/contact-1.jpg);
+  background-size: cover;
+  background-position: center;
+`;
+
+export default PrintContact;
