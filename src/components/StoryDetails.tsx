b389238ffd04a8c8d80432d922a9c5d72f23f8f9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 00:52:00 +0100

    feat: create StoryDetails component

---
+++ src/components/StoryDetails.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const StoryDetails = () => {
+  return <Container>StoryDetails</Container>;
+};
+
+const Container = styled.section`
+  background-color: orangered;
+`;
+
+export default StoryDetails;
