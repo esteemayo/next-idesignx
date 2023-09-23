8889cb6e594069fa42c9183501571d242b135bba
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 01:32:33 +0100

    feat: create Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Sidebar = () => {
+  return <Container>Sidebar</Container>;
+};
+
+const Container = styled.aside``;
+
+export default Sidebar;
