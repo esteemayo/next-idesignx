b142e2eecca81aec05d2b15d85008da966a6b4f4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 17:18:21 +0100

    feat: create PrintHero component

---
+++ src/components/hero/PrintHero.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const PrintHero = () => {
+  return <Header>PrintHero</Header>;
+};
+
+const Header = styled.header``;
+
+export default PrintHero;
