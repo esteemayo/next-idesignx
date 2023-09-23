56e6c19c6492f886a166101acf0267b498b94d5b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 02:54:03 +0100

    fix: add MenuItem component

--- src/components/sidebar/MenuItems.tsx
+++ src/components/sidebar/MenuItems.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import styled from 'styled-components';
+import MenuItem from './MenuItem';
 
 const MenuItems = () => {
-  return <Container>MenuItems</Container>;
+  return (
+    <Container>
+      <MenuItem />
+    </Container>
+  );
 };
 
 const Container = styled.ul``;
