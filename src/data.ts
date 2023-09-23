83b640fe83e181a794215d824f603eebc17d141f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 20:35:58 +0100

    fix: add galleryImages items

--- src/data.ts
+++ src/data.ts
@@ -94,3 +94,54 @@ export const printItems = [
     icon: faBagShopping,
   },
 ];
+
+export const galleryImages = [
+  {
+    id: 1,
+    img: 'img/gallery-1.jpg',
+  },
+  {
+    id: 2,
+    img: 'img/gallery-2.jpg',
+  },
+  {
+    id: 3,
+    img: 'img/gallery-3.jpg',
+  },
+  {
+    id: 4,
+    img: 'img/gallery-4.jpg',
+  },
+  {
+    id: 5,
+    img: 'img/gallery-5.jpg',
+  },
+  {
+    id: 6,
+    img: 'img/gallery-6.jpg',
+  },
+  {
+    id: 7,
+    img: 'img/gallery-7.jpg',
+  },
+  {
+    id: 8,
+    img: 'img/gallery-8.jpg',
+  },
+  {
+    id: 9,
+    img: 'img/gallery-9.jpg',
+  },
+  {
+    id: 10,
+    img: 'img/gallery-10.jpg',
+  },
+  {
+    id: 11,
+    img: 'img/gallery-11.jpg',
+  },
+  {
+    id: 12,
+    img: 'img/gallery-12.jpg',
+  },
+];
