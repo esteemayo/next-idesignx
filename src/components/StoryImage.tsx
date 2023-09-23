df29b6707b25bd5903cbabe180e47b29c79578ad
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 00:53:42 +0100

    feat: create StoryImage component

---
+++ src/components/StoryImage.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const StoryImage = () => {
+  return <Container>StoryImage</Container>;
+};
+
+const Container = styled.section`
+  background-color: greenyellow;
+`;
+
+export default StoryImage;
