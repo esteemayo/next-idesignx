91d22d1d36fe91314f0d7637222889a6131e0a04
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 02:53:25 +0100

    feat: create MenuItem component

---
+++ src/components/sidebar/MenuItem.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const MenuItem = () => {
+  return <ListItem>MenuItem</ListItem>;
+};
+
+const ListItem = styled.li``;
+
+export default MenuItem;
