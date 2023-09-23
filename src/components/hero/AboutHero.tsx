8d7dd9bcf05f176401eb7d6fc6a110b3808dcacd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 00:48:40 +0100

    feat: create AboutHero component

---
+++ src/components/hero/AboutHero.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import styled from 'styled-components';
+
+const AboutHero = () => {
+  return <Header>AboutHero</Header>;
+};
+
+const Header = styled.header`
+  background-color: crimson;
+`;
+
+export default AboutHero;
