3d3cc7045d8fcfc81b9eb91b5be770cd656e991a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 00:46:47 +0100

    fix: modify

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,20 @@
+'use client';
+
+import styled from 'styled-components';
+
 const About = () => {
-  return <div>About</div>;
+  return <Container>About</Container>;
 };
+
+const Container = styled.main`
+  display: grid;
+  grid-template-rows: 60vh 40vw min-content 20vw min-content;
+  grid-template-columns:
+    [full-start]minmax(6rem, 1fr) [center-start] repeat(
+      8,
+      [col-start] minmax(min-content, 15rem) [col-end]
+    )
+    [center-end] minmax(6rem, 1fr) [full-end];
+`;
 
 export default About;
