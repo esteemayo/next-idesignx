20ec810b08827c224b9a193592be173797d51356
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 02:41:49 +0100

    fix: refactor

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import { usePathname } from 'next/navigation';
 import Logo from './Logo';
 import Hamburger from './Hamburger';
 import NavItems from './NavItems';
+import Button from '../buttons/Button';
 
 import { navItems } from '@/data';
 
@@ -22,7 +23,7 @@ const Navbar = () => {
       </Wrapper>
       <NavItems links={navItems} path={pathname} />
       <Box>
-        <Button>Upload your design</Button>
+        <Button nav type='button' label='Upload your design' />
       </Box>
     </Container>
   );
@@ -54,32 +55,32 @@ const Box = styled.div`
   justify-self: end;
 `;
 
-const Button = styled.button`
-  display: inline-block;
-  border: none;
-  font-weight: 400;
-  font-size: 1.5rem;
-  padding: 1rem 3rem;
-  background-image: linear-gradient(
-    to right bottom,
-    var(--clr-purple-dark-1),
-    var(--clr-purple-light-1),
-    var(--clr-purple-light-2),
-    var(--clr-purple-light-3)
-  );
-  color: var(--clr-white);
-  border-radius: 0.5rem;
-  outline-color: var(--clr-purple-light-3);
-  cursor: pointer;
-  transition: all 0.2s;
+// const Button = styled.button`
+//   display: inline-block;
+//   border: none;
+//   font-weight: 400;
+//   font-size: 1.5rem;
+//   padding: 1rem 3rem;
+//   background-image: linear-gradient(
+//     to right bottom,
+//     var(--clr-purple-dark-1),
+//     var(--clr-purple-light-1),
+//     var(--clr-purple-light-2),
+//     var(--clr-purple-light-3)
+//   );
+//   color: var(--clr-white);
+//   border-radius: 0.5rem;
+//   outline-color: var(--clr-purple-light-3);
+//   cursor: pointer;
+//   transition: all 0.2s;
 
-  &:hover {
-    opacity: 0.9;
-  }
+//   &:hover {
+//     opacity: 0.9;
+//   }
 
-  &:disabled {
-    opacity: 0.75;
-  }
-`;
+//   &:disabled {
+//     opacity: 0.75;
+//   }
+// `;
 
 export default Navbar;
