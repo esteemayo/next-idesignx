4defb7316df74fae70bc895dd27dc038bfc12216
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 00:58:16 +0100

    feat: create AboutCard component

---
+++ src/components/AboutCard.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const AboutCard = () => {
+  return <Container>AboutCard</Container>;
+};
+
+const Container = styled.section`
+  background-color: palevioletred;
+`;
+
+export default AboutCard;
