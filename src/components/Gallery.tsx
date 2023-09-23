b7ab1ad9dd39a4dffdfa7cf38236091d041dc3db
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 17:20:55 +0100

    feat: create Gallery component

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Gallery = () => {
+  return <Container>Gallery</Container>;
+};
+
+const Container = styled.section``;
+
+export default Gallery;
