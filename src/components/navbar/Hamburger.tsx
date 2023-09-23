aaf7ea1e9ed3e09f7f8aa1cfe2b48d776db039ff
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 16:35:25 +0100

    fix: modify

--- src/components/navbar/Hamburger.tsx
+++ src/components/navbar/Hamburger.tsx
@@ -43,7 +43,7 @@ const Container = styled.button`
   font-weight: 400;
   background-color: transparent;
   color: ${({ type }) =>
-    type === true ? 'var(--clr-white)' : 'var(--clr-purple-light-2)'};
+    type === 'true' ? 'var(--clr-white)' : 'var(--clr-purple-light-2)'};
   cursor: pointer;
 
   @media only screen and (min-width: 48em) {
@@ -58,16 +58,16 @@ const Span = styled.span`
   transform-origin: left;
 
   &:first-child {
-    transform: ${({ type }) => type === true && 'rotate(45deg)'};
+    transform: ${({ type }) => type === 'true' && 'rotate(45deg)'};
   }
 
   &:nth-child(2) {
-    opacity: ${({ type }) => type === true && 0};
-    visibility: ${({ type }) => type === true && 'hidden'};
+    opacity: ${({ type }) => type === 'true' && 0};
+    visibility: ${({ type }) => type === 'true' && 'hidden'};
   }
 
   &:last-child {
-    transform: ${({ type }) => type === true && 'rotate(-45deg)'};
+    transform: ${({ type }) => type === 'true' && 'rotate(-45deg)'};
   }
 `;
 
