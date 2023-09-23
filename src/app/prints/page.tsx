b0b44d1bdcfdef5cd88ed9d90eec5f9139376e86
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 17:19:14 +0100

    fix: add PrintHero component

--- src/app/prints/page.tsx
+++ src/app/prints/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import styled from 'styled-components';
+import PrintHero from '@/components/hero/PrintHero';
 
 const Prints = () => {
-  return <Container>Prints</Container>;
+  return (
+    <Container>
+      <PrintHero />
+    </Container>
+  );
 };
 
 const Container = styled.main`
