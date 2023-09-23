5c698308a8a0e627bcfec907c28126963942d05e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-23 01:34:10 +0100

    feat: add styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,6 +6,10 @@ const Sidebar = () => {
   return <Container>Sidebar</Container>;
 };
 
-const Container = styled.aside``;
+const Container = styled.aside`
+  width: 45rem;
+  height: 100%;
+  position: fixed;
+`;
 
 export default Sidebar;
