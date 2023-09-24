929772d22e76e7aadce2df1e5166a97e947c188d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 03:06:45 +0100

    fix: add styling

--- src/components/StoryCards.tsx
+++ src/components/StoryCards.tsx
@@ -18,6 +18,10 @@ const StoryCards = () => {
 const Container = styled.section`
   grid-column: center-start / center-end;
   margin: 15rem 0;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+  gap: 6rem;
 `;
 
 export default StoryCards;
