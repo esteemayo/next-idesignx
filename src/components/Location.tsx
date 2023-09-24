c1a69ce47403378dc98408274b6bb75ca1906811
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 14:46:57 +0100

    feat: create Location component

---
+++ src/components/Location.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Location = () => {
+  return <Container>Location</Container>;
+};
+
+const Container = styled.section`
+  background-color: yellowgreen;
+`;
+
+export default Location;
