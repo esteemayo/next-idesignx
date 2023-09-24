99805b299144d48a87e4a3607fad12a673598f20
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 01:49:29 +0100

    fix: add p element tag

--- src/components/StoryDetails.tsx
+++ src/components/StoryDetails.tsx
@@ -6,6 +6,16 @@ const StoryDetails = () => {
   return (
     <Container>
       <Heading>Who are we</Heading>
+      <Text>
+        We are a Creative Design, Brand Development and activation firm. We aim
+        to present brands in grand style. Focusing on each drop in a massive
+        rainfall is a way of life to us, we treat every details with caution to
+        set your brand in motion of excellence.
+      </Text>
+      <Text>
+        We treat every details with caution to set your brand in motion of
+        excellence.
+      </Text>
     </Container>
   );
 };
