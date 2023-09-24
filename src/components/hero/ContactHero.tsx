b5cdc48caf680fcb702d76b451c8d760cdf7f8a9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 14:39:19 +0100

    feat: create ContactHero component

---
+++ src/components/hero/ContactHero.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const ContactHero = () => {
+  return <Header>ContactHero</Header>;
+};
+
+const Header = styled.header`
+  background-color: crimson;
+`;
+
+export default ContactHero;
