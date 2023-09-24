a88693b84fd20fd3d55feb41bf1a7f0ff6be7b91
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 02:11:01 +0100

    fix: add img element tag

--- src/components/StoryImage.tsx
+++ src/components/StoryImage.tsx
@@ -4,7 +4,13 @@ import Image from 'next/image';
 import styled from 'styled-components';
 
 const StoryImage = () => {
-  return <Container>StoryImage</Container>;
+  return (
+    <Container>
+      <StyledImage src='/img/story-pic-1.jpg' width={287} height={182} alt='' />
+      <StyledImage src='/img/story-pic-2.jpg' width={287} height={182} alt='' />
+      <StyledImage src='/img/story-pic-3.jpg' width={287} height={182} alt='' />
+    </Container>
+  );
 };
 
 const Container = styled.section`
