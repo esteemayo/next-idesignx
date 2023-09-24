96e70db0bd0d638e39a3d3bf68189abdc580d598
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 01:00:35 +0100

    feat: create AboutImage component

---
+++ src/components/AboutImage.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const AboutImage = () => {
+  return <Container>AboutImage</Container>;
+};
+
+const Container = styled.section`
+  background-color: pink;
+`;
+
+export default AboutImage;
