7953d16df8e15d9cb6d11595fbb074620e679b5a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 14:35:57 +0100

    fix: add page layout

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,20 @@
+'use client';
+
+import styled from 'styled-components';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return <Container>Contact</Container>;
 };
+
+const Container = styled.main`
+  display: grid;
+  grid-template-rows: 60vh min-content;
+  grid-template-columns:
+    [full-start] minmax(6rem, 1fr) [center-start] repeat(
+      8,
+      [col-start] minmax(min-content, 15rem) [col-end]
+    )
+    [center-end] minmax(6rem, 1fr) [full-end];
+`;
 
 export default Contact;
