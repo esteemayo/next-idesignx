ec5e1b93ecc1f1bb413eb30a46ca4a6980733497
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 14:44:28 +0100

    fix: rename component

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const ContactForm = () => {
+  return <Container>ContactForm</Container>;
+};
+
+const Container = styled.section`
+  background-color: palevioletred;
+`;
+
+export default ContactForm;
