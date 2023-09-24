eb6d231529c0b15d49fec5138ab89b49036bad8c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 14:57:17 +0100

    fix: add styling

--- src/components/hero/ContactHero.tsx
+++ src/components/hero/ContactHero.tsx
@@ -15,7 +15,16 @@ const ContactHero = () => {
 
 const Header = styled.header`
   grid-column: full-start / full-end;
-  background-color: crimson;
+
+  background-image: linear-gradient(
+      to right bottom,
+      rgba(0, 0, 0, 0.7),
+      rgba(51, 51, 51, 0.7)
+    ),
+    url(../img/contact-hero.jpg);
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
 `;
 
 const Heading = styled.h1``;
