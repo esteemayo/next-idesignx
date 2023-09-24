8fefd1142d06ed5d3cf25c5c2c203836fb43fbaa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 02:54:12 +0100

    fix: add StoryCards component

---
+++ src/components/StoryCards.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import styled from 'styled-components';
+
+const StoryCards = () => {
+  return <Container>StoryCards</Container>;
+};
+
+const Container = styled.section`
+  grid-column: center-start / center-end;
+  margin: 15rem 0;
+`;
+
+export default StoryCards;
