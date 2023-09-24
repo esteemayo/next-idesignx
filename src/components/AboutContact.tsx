bde94417dc533b8984593254a98e5f479d65e46b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-24 01:02:59 +0100

    feat: create AboutContact component

---
+++ src/components/AboutContact.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const AboutContact = () => {
+  return <Container>AboutContact</Container>;
+};
+
+const Container = styled.section`
+  background-color: purple;
+`;
+
+export default AboutContact;
