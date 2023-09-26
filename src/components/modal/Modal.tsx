00f682e26fb4f0cb4df356c4646376cd3f5092cd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-26 03:23:45 +0100

    fix: add styling

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -12,6 +12,10 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onOpen, onClose }) => {
   return <Container>Modal</Container>;
 };
 
-const Container = styled.aside``;
+const Container = styled.aside`
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
 
 export default Modal;
