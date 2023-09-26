2062df430f0bff61ed19005bd36c3d10f1adb062
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-26 03:15:17 +0100

    feat: create DesignModal component

---
+++ src/components/modal/DesignModal.tsx
@@ -0,0 +1,11 @@
+'use  client';
+
+import styled from 'styled-components';
+
+const DesignModal = () => {
+  return <Container>DesignModal</Container>;
+};
+
+const Container = styled.div``;
+
+export default DesignModal;
