3da8d18187d98e0a9936d8e120e7f8c6ea506f14
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-26 19:32:17 +0100

    fix: add styling

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -31,10 +31,9 @@ const Container = styled.aside`
   width: 100vw;
   height: 100%;
   background-color: rgba(38, 38, 38, 0.7);
-  display: ${({ show }) => (show === 'true' ? 'block' : 'none')};
-  opacity: ${({ show }) => (show === 'true' ? 1 : 0)};
-  visibility: ${({ show }) => (show === 'true' ? 'visible' : 'hidden')};
-  z-index: ${({ show }) => (show === 'true' ? 5000 : -1)};
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default Modal;
