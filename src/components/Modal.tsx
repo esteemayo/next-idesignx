7ef87dc8768ffdceb7111fe3f3c5cb0c103da516
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-26 02:49:00 +0100

    feat: create Modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Modal = () => {
+  return <Container>Modal</Container>;
+};
+
+const Container = styled.aside``;
+
+export default Modal;
