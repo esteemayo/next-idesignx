416b1ccdfd54de8ec5b4dc3fb07f008d86193307
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-26 19:32:50 +0100

    fix: add styling

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -31,6 +31,8 @@ const Container = styled.aside`
   width: 100vw;
   height: 100%;
   background-color: rgba(38, 38, 38, 0.7);
+  z-index: 5000;
+
   display: flex;
   align-items: center;
   justify-content: center;
