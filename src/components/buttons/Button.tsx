1ef0b30c8a32422a2552b9554a11595a3eb3ec95
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-27 00:45:42 +0100

    fix: add styling

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 interface ButtonProps {
   label: string;
@@ -44,13 +44,17 @@ const StyledButton = styled.button`
   padding: 1rem 3rem;
   font-weight: 400;
   font-size: 1.5rem;
-  background-image: linear-gradient(
+  background-image: ${({ outline }) =>
+    !outline &&
+    css`
+  linear-gradient(
     to right bottom,
     var(--clr-purple-dark-1),
     var(--clr-purple-light-1),
     var(--clr-purple-light-2),
     var(--clr-purple-light-3)
-  );
+  )
+  `};
   color: var(--clr-white);
   border-radius: 0.5rem;
   outline-color: var(--clr-purple-light-3);
