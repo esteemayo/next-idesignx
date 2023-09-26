60e5a9149a9677870efb8aee32b9b9ede407bf93
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-26 02:34:51 +0100

    fix: pass data via props

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     <Container active={activeMenu}>
       <Wrapper>
         <Hamburger isOpen={isOpen} onToggle={onOpen} />
-        <Logo />
+        <Logo onClose={handleClose} />
       </Wrapper>
       <NavItems links={navItems} path={pathname} />
       <Box>
