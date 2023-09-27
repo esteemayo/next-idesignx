dbf78fc49fad0508ea5acf33fb6f9ec0e727cf53
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-27 01:37:11 +0100

    fix: modify

--- src/components/modal/DesignModal.tsx
+++ src/components/modal/DesignModal.tsx
@@ -8,14 +8,7 @@ import { useDesignModal } from '@/hooks/useDesignModal';
 const DesignModal = () => {
   const { isOpen, onClose, onOpen } = useDesignModal();
 
-  return (
-    <Modal
-      title='Upload your design'
-      isOpen={isOpen}
-      onOpen={onOpen}
-      onClose={onClose}
-    />
-  );
+  return <Modal title='Upload your design' isOpen={isOpen} onClose={onClose} />;
 };
 
 const Container = styled.div``;
