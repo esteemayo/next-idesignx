8124102e1974b6f817713c84c0aacc3ee178c816
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-28 00:58:40 +0100

    feat: create Heading component

---
+++ src/components/Heading.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Heading = () => {
+  return <Container>Heading</Container>;
+};
+
+const Container = styled.div``;
+
+export default Heading;
