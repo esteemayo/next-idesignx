c7633667b98ef76f9adcde0e0b87de52e65fcf7a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-27 16:43:58 +0100

    feat: create TextAreaStyled component

---
+++ src/components/form/TextAreaStyled.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const TextAreaStyled = styled.textarea`
+  width: 100%;
+  display: inline-block;
+  resize: none;
+  font-family: inherit;
+  font-size: 1.5rem;
+  padding: 2rem;
+  background-color: var(--clr-white);
+  color: var(--clr-gray-dark-2);
+  border: 2px solid #aaa;
+  outline-color: #eee;
+  caret-color: var(--clr-purple-light-3);
+  transition: all 0.3s;
+
+  &::placeholder {
+    font-size: 1.4rem;
+    color: #bbb;
+  }
+`;
