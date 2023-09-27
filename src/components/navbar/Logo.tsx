fdd9556a1279efa5d33b32db115eb004b6a0c2ef
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-27 14:07:35 +0100

    fix: modify

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -10,8 +10,8 @@ interface LogoProps {
 
 const Logo: React.FC<LogoProps> = ({ onClose }) => {
   return (
-    <Container>
-      <StyledLink href='/' onClick={onClose}>
+    <Container onClick={onClose}>
+      <StyledLink href='/'>
         <StyledImage src='/img/logo.png' width={45} height={45} alt='logo' />
       </StyledLink>
     </Container>
