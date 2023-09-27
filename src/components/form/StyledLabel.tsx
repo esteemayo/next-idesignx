fb48a11a139a0ba2f80d2b2eb90ea816ca1825fe
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-28 00:45:40 +0100

    feat: create StyledLabel component

---
+++ src/components/form/StyledLabel.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledLabel = styled.label`
+  display: block;
+  font-weight: 400;
+  font-size: 1.4rem;
+  color: var(--clr-gray-dark-2);
+  margin-bottom: 1rem;
+`;
