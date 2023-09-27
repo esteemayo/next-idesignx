4295a4bdba418088ad62283de4f30898ce6f8c2a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-27 15:30:46 +0100

    fix: add step state variable

--- src/components/modal/DesignModal.tsx
+++ src/components/modal/DesignModal.tsx
@@ -1,12 +1,20 @@
 'use  client';
 
 import styled from 'styled-components';
+import { useState } from 'react';
 
 import Modal from './Modal';
 import { useDesignModal } from '@/hooks/useDesignModal';
 
+enum STEPS {
+  INFO = 0,
+  IMAGES = 1,
+}
+
 const DesignModal = () => {
   const { isOpen, onClose } = useDesignModal();
+
+  const [step, setStep] = useState(STEPS.INFO);
 
   return <Modal title='Upload your design' isOpen={isOpen} onClose={onClose} />;
 };
