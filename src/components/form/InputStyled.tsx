326f2f8ef840be2b76ac4983c10ae411962dcd3a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-27 16:41:21 +0100

    fix: change file directory

---
+++ src/components/form/InputStyled.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const InputStyled = styled.input`
+  width: 100%;
+  display: inline-block;
+  font-family: inherit;
+  font-size: 1.5rem;
+  padding: 1.5rem 2rem;
+  background-color: var(--clr-white);
+  color: var(--clr-gray-dark-2);
+  border: 2px solid #aaa;
+  border-radius: 2px;
+  outline-color: #eee;
+  caret-color: var(--clr-purple-light-3);
+  transition: all 0.3s;
+
+  &::placeholder {
+    font-size: 1.4rem;
+    color: #bbb;
+  }
+`;
