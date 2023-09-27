a8af6632b0d937070d1bfe15f074603db3964314
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-28 00:50:47 +0100

    fix: add styling

--- src/components/form/StyledLabel.tsx
+++ src/components/form/StyledLabel.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledLabel = styled.label`
-  display: block;
+  display: inline-block;
   font-weight: 500;
   font-size: 1.4rem;
   color: var(--clr-gray-dark-2);
