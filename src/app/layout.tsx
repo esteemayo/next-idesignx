402258dffde0bf6c94c7f35d670def4a3bb2d392
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-29 00:59:14 +0100

    fix: change to nunito fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Nunito } from 'next/font/google';
 
 import Layout from '@/components/Layout';
 
 import './globals.scss';
 
-const inter = Inter({ subsets: ['latin'] });
+const nunito = Nunito({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className={nunito.className}>
         <Layout>{children}</Layout>
       </body>
     </html>
