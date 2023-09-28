9985c6e5eb71d66e10c409952fb8f0def4e74d3e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-28 01:58:55 +0100

    feat: create ImageUpload component

---
+++ src/components/inputs/ImageUpload.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const ImageUpload = () => {
+  return <Container>ImageUpload</Container>;
+};
+
+const Container = styled.div``;
+
+export default ImageUpload;
