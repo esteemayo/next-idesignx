fa81b4bde59e15699f43152b36ae79642089e365
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-28 16:42:25 +0100

    fix: refactor

--- src/components/form/TextAreaStyled.tsx
+++ src/components/form/TextAreaStyled.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export const TextAreaStyled = styled.textarea`
+interface ITextArea {
+  error?: string | undefined;
+}
+
+export const TextAreaStyled = styled.textarea<ITextArea>`
   width: 100%;
   display: inline-block;
   resize: none;
@@ -9,13 +13,29 @@ export const TextAreaStyled = styled.textarea`
   padding: 2rem;
   background-color: var(--clr-white);
   color: var(--clr-gray-dark-2);
-  border: 2px solid #aaa;
-  outline-color: #979797;
-  caret-color: var(--clr-purple-light-3);
+  border: 2px solid ${({ error }) => setBorderColor(error)};
+  outline-color: ${({ error }) => setOutlineColor(error)};
+  caret-color: ${({ error }) => setCaretColor(error)};
   transition: all 0.3s;
 
   &::placeholder {
     font-size: 1.4rem;
-    color: #bbb;
+    color: ${({ error }) => setPlaceholderColor(error)};
   }
 `;
+
+const setBorderColor = (error: string | undefined) => {
+  return error === 'true' ? '#f00' : '#aaa';
+};
+
+const setOutlineColor = (error: string | undefined) => {
+  return error === 'true' ? '#f00' : '#979797';
+};
+
+const setCaretColor = (error: string | undefined) => {
+  return error === 'true' ? '#f00' : 'var(--clr-purple-light-3)';
+};
+
+const setPlaceholderColor = (error: string | undefined) => {
+  return error === 'true' ? '#f00' : '#bbb';
+};
