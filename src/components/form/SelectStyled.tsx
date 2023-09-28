3cd26ce83c6e0473617d499451f12df8012bf0f8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-28 14:52:21 +0100

    feat: create SelectStyled component

---
+++ src/components/form/SelectStyled.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const SelectStyled = styled.select`
+  width: 100%;
+  display: inline-block;
+  font-family: inherit;
+  font-size: 1.5rem;
+  padding: 1.5rem 2rem;
+  background-color: transparent;
+  color: var(--clr-gray-dark-2);
+  border: 2px solid #aaa;
+  border-radius: 2px;
+`;
+
+export const Option = styled.option`
+  font-size: 1.4rem;
+  color: #bbb;
+  letter-spacing: 0.5rem;
+`;
