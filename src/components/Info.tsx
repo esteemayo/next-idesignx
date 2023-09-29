f3b4a5b3d88eed6c0cc6975c51f1d5b0400d131a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-29 03:21:44 +0100

    fix: add styling

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -83,8 +83,7 @@ const EmailAddress = styled.span``;
 const SocialWrapper = styled.div`
   align-self: center;
   justify-self: self-end;
-  /* width: 1.3rem;
-  height: 1.3rem; */
+
   margin-right: 0.5rem;
   display: flex;
   align-items: center;
