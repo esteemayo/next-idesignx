bc59914b059be96fa05ad5e5cbc82d1b230907df
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-30 01:12:53 +0100

    fix: modify

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,9 +3,9 @@
 import styled from 'styled-components';
 
 import ProductImage from './ProductImage';
+import { StyledLink } from './buttons/StyledLink';
 
 import { productImages } from '@/data';
-import { StyledLink } from './buttons/StyledLink';
 
 const Products = () => {
   return (
