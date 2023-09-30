fc62d9d6a57beae55c193d6552fee1003ffca1d9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-30 13:29:05 +0100

    fix: add media query

--- src/app/prints/page.tsx
+++ src/app/prints/page.tsx
@@ -34,6 +34,10 @@ const Container = styled.main`
       [col-start] minmax(min-content, 15rem) [col-end]
     )
     [center-end] minmax(6rem, 1fr) [full-end];
+
+  @media only screen and (max-width: 50em) {
+    grid-template-rows: 60vh;
+  }
 `;
 
 export default Prints;
