a40241325df29dd308f6d1d6587b3761f5af78ef
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-30 01:46:31 +0100

    fix: add media query

--- src/components/form/FormGroup.tsx
+++ src/components/form/FormGroup.tsx
@@ -3,5 +3,9 @@ import styled from 'styled-components';
 export const FormGroup = styled.div`
   &:not(:last-of-type) {
     margin-bottom: 2rem;
+
+    @media only screen and (max-width: 50em) {
+      margin-bottom: 1.5rem;
+    }
   }
 `;
