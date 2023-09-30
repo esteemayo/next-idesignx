283e88c263a3934f28b18607a3c9b0b2238f1e93
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-30 15:20:11 +0100

    fix: modify

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -68,7 +68,7 @@ const StyledImage = styled(Image)`
   display: block;
   object-fit: cover;
 
-  @media only screen and (max-width: 25em) {
+  @media only screen and (max-width: 26.25em) {
     width: 40rem;
     height: 30rem;
   }
