5d8e6ed9ead4839d37c52c2690e30d0555e23376
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-04 20:05:44 +0100

    fix: add dark/light mode styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,36 +1,52 @@
 'use client';
 
 import { useMemo } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import CopyRight from './CopyRight';
 import MenuLinks from './MenuLinks';
 
 import { menuItems } from '@/data';
+import { useDarkMode } from '@/hooks/useDarkMode';
+
+interface IContainer {
+  mode: string;
+}
 
 const Footer = () => {
+  const mode = useDarkMode((state) => state.mode);
+
   const copyRightLabel = useMemo(() => {
     const now = new Date();
     return now.getFullYear();
   }, []);
 
+  const activeMode = useMemo(() => {
+    return mode.toString();
+  }, [mode]);
+
   return (
-    <Container>
+    <Container mode={activeMode}>
       <MenuLinks links={menuItems} />
       <CopyRight value={copyRightLabel} />
     </Container>
   );
 };
 
-const Container = styled.footer`
+const Container = styled.footer<IContainer>`
   padding: 8rem;
-  background-image: linear-gradient(
+  background-color: ${({ mode }) => mode === 'true' && '#101418'};
+  background-image: ${({ mode }) =>
+    mode === 'false' &&
+    css`
+  linear-gradient(
     to right bottom,
     var(--clr-purple-dark-1),
     var(--clr-purple-light-1),
     var(--clr-purple-light-2),
     var(--clr-purple-light-3)
-  );
+  )
+  `};
   color: var(--clr-white);
 
   @media only screen and (max-width: 37.5em) {
