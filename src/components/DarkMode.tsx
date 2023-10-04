1c1955d913a120f3f77697ccd49d096c92182d76
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-04 02:57:00 +0100

    feat: create v component

---
+++ src/components/DarkMode.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const DarkMode = () => {
+  return <Container>DarkMode</Container>;
+};
+
+const Container = styled.div``;
+
+export default DarkMode;
