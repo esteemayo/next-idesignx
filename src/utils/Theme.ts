3ce2bba9a06bb0fb57af07bdb76c75ca538a2cf0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-04 15:10:15 +0100

    fix: add color values

--- src/utils/Theme.ts
+++ src/utils/Theme.ts
@@ -1,13 +1,15 @@
 export const darkTheme = {
   bg: '#141a1f',
   bgLighter: '#101418',
+  bgInput: '#101418',
   text: 'var(--clr-white)',
-  textSoft: '#b0b8C4'
+  textSoft: '#b0b8C4',
 };
 
 export const lightTheme = {
   bg: 'var(--clr-white)',
   bgLighter: '#f9f9f9',
+  bgInput: 'var(--clr-white)',
   text: 'var(--clr-purple-light-1)',
-  textSoft: 'inherit'
+  textSoft: 'inherit',
 };
