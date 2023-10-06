db8912bb8a4acb98b89ecdeada60ed458ff37413
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-07 00:08:50 +0100

    fix: add GalleryHero component

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 
+import GalleryHero from '@/components/hero/GalleryHero';
 import styled from 'styled-components';
 
 const Gallery = () => {
-  return <Container>Gallery</Container>;
+  return (
+    <Container>
+      <GalleryHero />
+    </Container>
+  );
 };
 
 const Container = styled.main``;
