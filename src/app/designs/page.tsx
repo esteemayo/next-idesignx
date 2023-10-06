a05052ddc45bca175b606cb01ff55de7923ea3db
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-06 01:19:15 +0100

    fix: modify

--- src/app/designs/page.tsx
+++ src/app/designs/page.tsx
@@ -30,7 +30,6 @@ const Designs = () => {
 };
 
 const Container = styled.main`
-  background-color: rgba(249, 249, 249, 0.1);
   background-color: #f9f9f9;
   display: grid;
   grid-template-rows: 80vh 40vw repeat(2, min-content);
