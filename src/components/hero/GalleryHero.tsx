7dd6c795dc16871ea15392a87f47a4caff2b344d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-07 00:32:38 +0100

    fix: add css styling

--- src/components/hero/GalleryHero.tsx
+++ src/components/hero/GalleryHero.tsx
@@ -8,7 +8,16 @@ const GalleryHero = () => {
 
 const Header = styled.header`
   grid-column: full-start / full-end;
-  background-color: crimson;
+
+  background-image: linear-gradient(
+      to right bottom,
+      rgba(0, 0, 0, 0.7),
+      rgba(51, 51, 51, 0.7)
+    ),
+    url(/img/gallery-hero.jpg);
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
 `;
 
 export default GalleryHero;
