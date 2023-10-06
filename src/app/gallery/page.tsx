c2985df6705552f94dc198020fea29c1e507baa9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-07 00:20:19 +0100

    fix: add css styling

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -16,6 +16,13 @@ const Gallery = () => {
 
 const Container = styled.main`
   background-color: ${({ theme }) => theme.bg};
+
+  display: grid;
+  grid-template-rows: 70vh min-content;
+  grid-template-columns: [full-start] minmax(6rem, 1fr) [center-start] repeat(
+      8,
+      [col-start] minmax(min-content, 15rem) [col-end]
+    ) [center-end] minmax(6rem, 1fr) [full-end];
 `;
 
 export default Gallery;
