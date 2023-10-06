1806727dca7f36e4f35092d72a723ff93c18b806
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-07 00:12:27 +0100

    feat: create ImageGallery component

---
+++ src/components/ImageGallery.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const ImageGallery = () => {
+  return <Container>ImageGallery</Container>;
+};
+
+const Container = styled.section``;
+
+export default ImageGallery;
