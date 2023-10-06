33f62c51b964689fc339affa3937702f0bac2b1a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-06 23:50:31 +0100

    feat: create Galeey page component

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Gallery = () => {
+  return <Container>Gallery</Container>;
+};
+
+const Container = styled.main``;
+
+export default Gallery;
