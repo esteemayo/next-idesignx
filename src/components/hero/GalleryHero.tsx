61c8fc2b41c823fc0d804f6502e402fbdd185cf7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-07 00:08:18 +0100

    feat: create GalleryHero component

---
+++ src/components/hero/GalleryHero.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const GalleryHero = () => {
+  return <Header>GalleryHero</Header>;
+};
+
+const Header = styled.header``;
+
+export default GalleryHero;
