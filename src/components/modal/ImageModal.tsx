340046a0e2b6387f292fdf3d7058fecab345ff5a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-07 13:06:41 +0100

    fix: add css styling

--- src/components/modal/ImageModal.tsx
+++ src/components/modal/ImageModal.tsx
@@ -6,6 +6,10 @@ const ImageModal = () => {
   return <Overlay>ImageModal</Overlay>;
 };
 
-const Overlay = styled.aside``;
+const Overlay = styled.aside`
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
 
 export default ImageModal;
