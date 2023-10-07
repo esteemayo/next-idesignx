c67781efa9eb72912a67f0a38455d1559f5a90d2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-07 03:55:55 +0100

    feat: create ImageModal component

---
+++ src/components/modal/ImageModal.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const ImageModal = () => {
+  return <Overlay>ImageModal</Overlay>;
+};
+
+const Overlay = styled.aside``;
+
+export default ImageModal;
