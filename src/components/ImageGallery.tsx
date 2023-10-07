bff15886ff9b8a29b5ba56aee338dc24fbafda78
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-07 03:00:11 +0100

    fix: add css styling

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -21,12 +21,14 @@ const ImageGallery = () => {
 };
 
 const Container = styled.section`
+  padding: 1.5rem;
   background-color: var(--clr-gray-light-1);
 
   grid-column: full-start / full-end;
   display: grid;
   grid-template-rows: repeat(7, 5vw);
   grid-template-columns: repeat(8, 1fr);
+  gap: 1.5rem;
 `;
 
 const Wrapper = styled.figure``;
