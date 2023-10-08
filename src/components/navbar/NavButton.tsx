1e75529e6e9a52fd6a027fd426238ec58f592f3d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-08 14:43:02 +0100

    feat: create NavButton component

---
+++ src/components/navbar/NavButton.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const NavButton = () => {
+  return <Button>NavButton</Button>;
+};
+
+const Button = styled.button``;
+
+export default NavButton;
