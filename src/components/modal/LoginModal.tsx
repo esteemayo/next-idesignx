a5cb7d570d11ec0227d5bb060a46a16d0d83266a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-09 01:28:52 +0100

    fix: add css styling

--- src/components/modal/LoginModal.tsx
+++ src/components/modal/LoginModal.tsx
@@ -97,7 +97,8 @@ const Wrapper = styled.div`
 const Line = styled.span`
   width: 100%;
   height: 0.1px;
-  background-color: var(--clr-gray);
+  background-color: #d7d5d5;
+  margin-top: 2rem;
 `;
 
 export default LoginModal;
