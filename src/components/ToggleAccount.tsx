e0fee81372185f7090d6093c1ffc0982eb23e235
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-09 02:00:16 +0100

    fix: add css styling

--- src/components/ToggleAccount.tsx
+++ src/components/ToggleAccount.tsx
@@ -3,9 +3,45 @@
 import styled from 'styled-components';
 
 const ToggleAccount = () => {
-  return <Container>ToggleAccount</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Text>First time using iDesignx?</Text>
+        <Button>Create an account</Button>
+      </Wrapper>
+    </Container>
+  );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-weight: 300;
+  margin-top: 1.5rem;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.8rem;
+`;
+
+const Text = styled.p``;
+
+const Button = styled.button`
+  display: inline-block;
+  border: none;
+  font-size: inherit;
+  background-color: transparent;
+  color: inherit;
+  outline-color: var(--clr-gray-dark-2);
+  cursor: pointer;
+  transition: all 0.2s;
+
+  &:hover {
+    text-decoration: underline;
+    text-underline-offset: 0.5rem;
+  }
+`;
 
 export default ToggleAccount;
