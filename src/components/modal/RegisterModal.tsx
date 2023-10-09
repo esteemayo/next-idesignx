fd40ffce9082c1b832c10a1acc7982ccaff6c0ee
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-09 14:44:31 +0100

    fix: add data state variable

--- src/components/modal/RegisterModal.tsx
+++ src/components/modal/RegisterModal.tsx
@@ -1,13 +1,23 @@
 'use client';
 
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 import Modal from './Modal';
 import { useRegisterModal } from '@/hooks/useRegisterModal';
 
+const initialState = {
+  name: '',
+  email: '',
+  username: '',
+  password: '',
+  passwordConfirm: '',
+};
+
 const RegisterModal = () => {
   const isOpen = useRegisterModal((state) => state.isOpen);
   const onClose = useRegisterModal((state) => state.onClose);
+
+  const [data, setData] = useState(initialState);
 
   const handleSubmit = useCallback(() => {}, []);
 
