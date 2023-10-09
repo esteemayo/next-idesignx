f844caf1ec6f556f7a068fb939e9fc53f9c67438
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-09 13:27:33 +0100

    feat: create LoginOptions component

---
+++ src/components/LoginOptions.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import GoogleIcon from '@mui/icons-material/Google';
+import styled from 'styled-components';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+import Button from './buttons/Button';
+
+const LoginOptions = () => {
+  return (
+    <Container>
+      <Button
+        small='true'
+        outline='true'
+        type='button'
+        label='Login with Google'
+        icon={GoogleIcon}
+      />
+      <Button
+        small='true'
+        outline='true'
+        type='button'
+        label='Login with Github'
+        icon={GitHubIcon}
+      />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 3rem;
+
+  & > * {
+    width: 100%;
+  }
+`;
+
+export default LoginOptions;
