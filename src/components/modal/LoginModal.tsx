b750879e832851a642b5130b6fffb562cf10feaf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-10 00:38:33 +0100

    fix: modify

--- src/components/modal/LoginModal.tsx
+++ src/components/modal/LoginModal.tsx
@@ -55,11 +55,19 @@ const LoginModal = () => {
     const { email, password } = data;
 
     if (email.trim() === '') {
-      errors.email = 'true';
+      errors.email = 'Email must not be empty';
+    } else {
+      const regEx =
+        /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)*[a-zA-Z]{2,9})$/;
+      if (!email.match(regEx)) {
+        errors.email = 'Email must be a valid email address';
+      }
     }
 
-    if (password.trim() === '') {
-      errors.password = 'true';
+    if (password === '') {
+      errors.password = 'Password must not be empty';
+    } else if (password.length < 8) {
+      errors.password = 'Password should be at least 8 characters long';
     }
 
     return errors;
