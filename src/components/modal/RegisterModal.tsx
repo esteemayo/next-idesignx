e28e058a520c88b55ea7a4d81ff85ec11fb4843a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-09 15:00:08 +0100

    fix: add markup

--- src/components/modal/RegisterModal.tsx
+++ src/components/modal/RegisterModal.tsx
@@ -1,14 +1,19 @@
 'use client';
 
+import styled from 'styled-components';
 import { ChangeEvent, useCallback, useState } from 'react';
 
 import Modal from './Modal';
+import Heading from '../Heading';
+
+import Input from '../inputs/Input';
 import { useRegisterModal } from '@/hooks/useRegisterModal';
 
 const initialState = {
   name: '',
   email: '',
   username: '',
+  phone: '',
   password: '',
   passwordConfirm: '',
 };
@@ -36,14 +41,56 @@ const RegisterModal = () => {
     handleClear();
   }, [data, handleClear]);
 
+  let bodyContent;
+
+  bodyContent = (
+    <Container>
+      <Heading title='Welcome to iDesignx' subtitle='Create an account!' />
+      <Input
+        name='name'
+        label='Name'
+        value={data.name}
+        placeholder='Name'
+        onChange={handleChange}
+      />
+      <Input
+        name='email'
+        type='email'
+        label='Email address'
+        value={data.email}
+        placeholder='you@user.com'
+        onChange={handleChange}
+      />
+      <Input
+        name='username'
+        label='Username'
+        value={data.username}
+        placeholder='Username'
+        onChange={handleChange}
+      />
+      <Input
+        name='phone'
+        type='tel'
+        label='Phone number'
+        value={data.phone}
+        placeholder='Phone number'
+        onChange={handleChange}
+      />
+    </Container>
+  );
+
   return (
     <Modal
       isOpen={isOpen}
       title='Register'
+      actionLabel='Register'
       onClose={onClose}
+      body={bodyContent}
       onSubmit={handleSubmit}
     />
   );
 };
+
+const Container = styled.div``;
 
 export default RegisterModal;
