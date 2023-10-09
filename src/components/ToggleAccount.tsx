2f55d38d87ea66b37885cac945957813b2d8d7ac
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-09 01:58:58 +0100

    feat: create ToggleAccount component

---
+++ src/components/ToggleAccount.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const ToggleAccount = () => {
+  return <Container>ToggleAccount</Container>;
+};
+
+const Container = styled.div``;
+
+export default ToggleAccount;
