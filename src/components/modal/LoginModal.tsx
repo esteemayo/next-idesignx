19b4bae956d430550ceff0bd7fa1d1cf01475ad5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-09 01:18:33 +0100

    fix: add hr element tag

--- src/components/modal/LoginModal.tsx
+++ src/components/modal/LoginModal.tsx
@@ -62,10 +62,13 @@ const LoginModal = () => {
   );
 
   const footerContent = (
-    <Wrapper>
-      <Button outline='true' type='button' label='Login with Google' />
-      <Button outline='true' type='button' label='Login with Github' />
-    </Wrapper>
+    <>
+      <Hr />
+      <Wrapper>
+        <Button outline='true' type='button' label='Login with Google' />
+        <Button outline='true' type='button' label='Login with Github' />
+      </Wrapper>
+    </>
   );
 
   return (
