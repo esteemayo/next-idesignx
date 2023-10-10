17087bcf1ce2a39551d616b9e8699f27594ae924
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-10 17:10:50 +0100

    fix: change image directory

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -27,7 +27,7 @@ const Header = styled.header`
       rgba(0, 0, 0, 0.7),
       rgba(51, 51, 51, 0.7)
     ),
-    url(/img/hero.jpg);
+    url(/img/hero/hero.jpg);
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
