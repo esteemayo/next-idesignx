09c0e1af57052fc81677ba1c86a15806f5ece6ba
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-12 00:56:38 +0100

    feat: create NotFound component

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import styled from 'styled-components';
+
+const NotFound = () => {
+  return <Container>NotFound</Container>;
+};
+
+const Container = styled.main``;
+
+export default NotFound;
