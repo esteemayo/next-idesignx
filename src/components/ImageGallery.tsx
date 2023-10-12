66cee3daeb20e20e7c5c040e822b2b6110381d58
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-12 19:51:59 +0100

    fix: refactor

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -14,59 +14,21 @@ type ImageValues = {
 };
 
 interface ImageGalleryProps {
-  data: ImageValues[];
+  images: ImageValues[];
+  onOpen(value: number): void;
 }
 
-const ImageGallery: FC<ImageGalleryProps> = ({ data }) => {
-  const { isOpen, onOpen, onClose } = useImageModal();
-
-  const [images, setImages] = useState(data);
-  const [slideIndex, setSlideIndex] = useState(0);
-
-  const handleOpen = useCallback(
-    (index: number) => {
-      onOpen();
-      setSlideIndex(index);
-    },
-    [onOpen]
-  );
-
-  const handleClick = useCallback(
-    (direction: string) => {
-      let newSlideIndex;
-      const lastIndex = images.length - 1;
-
-      if (direction === 'left') {
-        newSlideIndex = slideIndex === 0 ? lastIndex : slideIndex - 1;
-      } else {
-        newSlideIndex = slideIndex === lastIndex ? 0 : slideIndex + 1;
-      }
-
-      setSlideIndex(newSlideIndex);
-    },
-    [images, slideIndex]
-  );
-
-  const selectedImage = useMemo(() => {
-    return images[slideIndex].img;
-  }, [images, slideIndex]);
-
+const ImageGallery: FC<ImageGalleryProps> = ({ images, onOpen }) => {
   return (
     <>
       <Container>
         {images.map((item, index) => {
           const { id, img } = item;
           return (
-            <ImageItem key={id} src={img} value={index} onClick={handleOpen} />
+            <ImageItem key={id} src={img} value={index} onClick={onOpen} />
           );
         })}
       </Container>
-      <ImageModal
-        image={selectedImage}
-        isOpen={isOpen}
-        onClose={onClose}
-        onClick={handleClick}
-      />
     </>
   );
 };
