556219db225b21d628d17f36c563ae8e07969235
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-12 16:43:29 +0100

    feat: create GalleryItem component

---
+++ src/components/GalleryItem.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { FC } from 'react';
+import styled from 'styled-components';
+
+const GalleryItem = () => {
+  return <Container>GalleryItem</Container>;
+};
+
+const Container = styled.figure``;
+
+export default GalleryItem;
