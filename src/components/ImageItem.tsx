7570258f8c8d5eeb1faa698cbe621937ca554f79
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-12 17:16:36 +0100

    fix: add img element tag

--- src/components/ImageItem.tsx
+++ src/components/ImageItem.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { FC } from 'react';
 import styled from 'styled-components';
 
@@ -14,5 +15,7 @@ const ImageItem: FC<ImageItemProps> = ({ src }) => {
 const Container = styled.figure`
   cursor: pointer;
 `;
+
+const StyledImage = styled(Image)``;
 
 export default ImageItem;
