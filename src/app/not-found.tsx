370e20d94fd422304b54ebf0f57ce7fe89ecf152
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-12 01:46:40 +0100

    fix: add css styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -26,6 +26,11 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  & > img {
+    width: 70rem;
+    height: 50rem;
+  }
 `;
 
 const StyledImage = styled(Image)``;
