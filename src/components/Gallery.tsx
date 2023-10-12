17518813d1d0bd3a38b34df5b4d0d8d83b3f9bec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-12 16:54:40 +0100

    fix: refactor

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -68,16 +68,4 @@ const ImageContainer = styled.figure`
   height: 100%;
 `;
 
-const StyledImage = styled(Image)`
-  width: 100%;
-  height: 100%;
-  display: block;
-  object-fit: cover;
-
-  @media only screen and (max-width: 26.25em) {
-    width: 40rem;
-    height: 30rem;
-  }
-`;
-
 export default Gallery;
