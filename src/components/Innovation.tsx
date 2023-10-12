816ec2bfd00011c9b94d70389b574340d44aeda2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-12 02:18:58 +0100

    feat: create Innovation component

---
+++ src/components/Innovation.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Innovation = () => {
+  return <Container>Innovation</Container>;
+};
+
+const Container = styled.section`
+  grid-column: full-start / full-end;
+`;
+
+export default Innovation;
