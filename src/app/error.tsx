1fda5191007a92a55860fd9b4c5867554dac95f9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-12 14:53:06 +0100

    fix: add media query

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -23,6 +23,10 @@ const Container = styled.main`
   width: 100%;
   height: 80vh;
   background-color: ${({ theme }) => theme.bg};
+
+  @media only screen and (max-width: 26.875em) {
+    height: 100vh;
+  }
 `;
 
 export default ErrorState;
