3e077f64cca2795af61e2a535aa077f3a6ceab13
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-12 17:15:25 +0100

    feat: add ImageItem component

---
+++ src/components/ImageItem.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { FC } from 'react';
+import styled from 'styled-components';
+
+interface ImageItemProps {
+  src: string;
+}
+
+const ImageItem: FC<ImageItemProps> = ({ src }) => {
+  return <Container>ImageItem</Container>;
+};
+
+const Container = styled.figure``;
+
+export default ImageItem;
