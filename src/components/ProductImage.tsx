848b0e218a63c83b00c3e5c880e1526b60591f24
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-13 02:15:08 +0100

    fix: modify

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -1,11 +1,14 @@
+'use client';
+
 import Image from 'next/image';
+import { FC } from 'react';
 import styled from 'styled-components';
 
 interface ProductImageProps {
   img: string;
 }
 
-const ProductImage: React.FC<ProductImageProps> = ({ img }) => {
+const ProductImage: FC<ProductImageProps> = ({ img }) => {
   return (
     <Wrapper>
       <StyledImage src={img} width={297} height={484} alt='' />
