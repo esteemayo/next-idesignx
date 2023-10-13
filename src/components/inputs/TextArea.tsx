6989df83b16fb1b3f49578a330481fdb9192ba74
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-13 20:45:45 +0100

    fix: refactor

--- src/components/inputs/TextArea.tsx
+++ src/components/inputs/TextArea.tsx
@@ -1,71 +1,29 @@
 'use client';
 
-import { ChangeEvent } from 'react';
+import { FC, InputHTMLAttributes } from 'react';
 
-import { Label } from '../form/Label';
+import { StyledLabel } from '../form/StyledLabel';
+import Error from '../form/Error';
 import { TextAreaStyled } from '../form/TextAreaStyled';
 import { FormGroup } from '../form/FormGroup';
-import { StyledTextArea } from '../form/StyledTextArea';
-import Error from '../form/Error';
-import { StyledLabel } from '../form/StyledLabel';
 
 import { useActiveMode } from '@/hooks/useActiveMode';
 
-interface TextAreaProps {
+interface TextAreaProps extends InputHTMLAttributes<HTMLTextAreaElement> {
   name: string;
   label: string;
-  value?: string;
-  placeholder: string;
-  style?: string;
-  path?: string;
-  onChange(e: ChangeEvent<HTMLInputElement>): void;
   error?: string;
 }
 
-const TextArea: React.FC<TextAreaProps> = ({
-  name,
-  label,
-  value,
-  placeholder,
-  style,
-  path,
-  onChange,
-  error,
-}) => {
+const TextArea: FC<TextAreaProps> = ({ name, label, error, ...rest }) => {
   const { activeMode } = useActiveMode();
-
-  if (style === 'true') {
-    return (
-      <FormGroup>
-        <StyledTextArea
-          id={name}
-          name={name}
-          value={value}
-          placeholder={placeholder}
-          onChange={onChange}
-          path={path}
-          required
-        />
-        <Label htmlFor={name}>{label}</Label>
-      </FormGroup>
-    );
-  }
 
   return (
     <FormGroup>
-      {label && (
-        <StyledLabel htmlFor={name} error={error} mode={activeMode}>
-          {label}
-        </StyledLabel>
-      )}
-      <TextAreaStyled
-        id={name}
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        error={error}
-      />
+      <StyledLabel htmlFor={name} error={error} mode={activeMode}>
+        {label}
+      </StyledLabel>
+      <TextAreaStyled {...rest} id={name} name={name} error={error} />
       {error && <Error message={error} />}
     </FormGroup>
   );
