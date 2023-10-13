bea775d25d3b9281372d47cda065a6c3a0a922af
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-13 16:17:36 +0100

    fix: modify

--- src/components/inputs/FormTextArea.tsx
+++ src/components/inputs/FormTextArea.tsx
@@ -9,12 +9,18 @@ import { StyledTextArea } from '../form/StyledTextArea';
 interface FormTextAreaProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   label: string;
+  path?: string;
 }
 
-const FormTextArea: FC<FormTextAreaProps> = ({ name, label, ...rest }) => {
+const FormTextArea: FC<FormTextAreaProps> = ({
+  name,
+  label,
+  path,
+  ...rest
+}) => {
   return (
     <FormGroup>
-      <StyledTextArea {...rest} id={name} name={name} required />
+      <StyledTextArea {...rest} id={name} name={name} path={path} required />
       <Label htmlFor={name}>{label}</Label>
     </FormGroup>
   );
