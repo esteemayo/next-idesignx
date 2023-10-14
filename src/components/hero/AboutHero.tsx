4361129f82d5fe2ec19e6675a1b09a0320d5307b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-14 19:37:34 +0100

    fix: modify

--- src/components/hero/AboutHero.tsx
+++ src/components/hero/AboutHero.tsx
@@ -16,6 +16,7 @@ const AboutHero = () => {
 
 const Header = styled.header`
   grid-column: full-start / full-end;
+
   display: grid;
   grid-template-rows: repeat(4, 1fr);
   grid-template-columns: repeat(6, 1fr);
