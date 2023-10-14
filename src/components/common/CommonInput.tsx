7bf1300aca7ebe29b70f6e81aefee835eec3f8c0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-14 17:41:07 +0100

    fix: change file directory

---
+++ src/components/common/CommonInput.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CommonInput = styled.input`
+  width: 100%;
+  display: inline-block;
+  font-family: inherit;
+  font-size: 1.5rem;
+  padding: 1.5rem 2rem;
+  color: var(--clr-gray-dark-2);
+  border-radius: 2px;
+  transition: all 0.3s;
+`;
