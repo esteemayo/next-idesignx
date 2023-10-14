37cb588e4a30c312dcecb01e04fb43ce96eb32c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-14 17:55:44 +0100

    feat: create CommonSelect component

---
+++ src/components/common/CommonSelect.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const CommonSelect = styled.select`
+  width: 100%;
+  display: inline-block;
+  font-family: inherit;
+  font-size: 1.5rem;
+  padding: 1.5rem 2rem;
+  border-radius: 2px;
+`;
