bf20baf6a464e676bdfc7e59549d2ef957054656
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-14 18:22:40 +0100

    fix: add css styling

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -6,5 +6,6 @@ export const CommonButton = styled.button`
   font-size: 1.5rem;
   border-radius: 0.5rem;
   outline-color: var(--clr-purple-light-3);
+  cursor: pointer;
   transition: all 0.2s;
 `;
