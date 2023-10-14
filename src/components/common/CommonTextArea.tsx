1f773b85eb394ebb68834c532ea9cd835b268e2a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-14 17:45:43 +0100

    feat: create CommonTextArea component

---
+++ src/components/common/CommonTextArea.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CommonTextArea = styled.textarea`
+  width: 100%;
+  display: inline-block;
+  resize: none;
+  font-family: inherit;
+  font-size: 1.5rem;
+  padding: 2rem;
+  color: var(--clr-gray-dark-2);
+  transition: all 0.3s;
+`;
