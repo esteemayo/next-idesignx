d27af1ef2f49ae238720160ffd1fd39c5dd920e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-14 18:17:35 +0100

    feat: create CommonButton component

---
+++ src/components/common/CommonButton.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const CommonButton = styled.button`
+  text-transform: none;
+  font-weight: 400;
+  font-size: 1.5rem;
+  border-radius: 0.5rem;
+  outline-color: var(--clr-purple-light-3);
+  transition: all 0.2s;
+`;
