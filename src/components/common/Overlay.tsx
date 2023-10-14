3150645310f0663d5fb0e25c4f760f21650ebf79
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-14 19:55:55 +0100

    feat: create Overlay component

---
+++ src/components/common/Overlay.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Overlay = styled.aside`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(38, 38, 38, 0.7);
+  z-index: 5000;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
