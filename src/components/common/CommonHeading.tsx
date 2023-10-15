84c529cc8401bf48f3a6b84778dd2fbeb3a939c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-15 02:45:08 +0100

    fix: add css styling

--- src/components/common/CommonHeading.tsx
+++ src/components/common/CommonHeading.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const CommonHeading = styled.h1`
+  font-family: var(--font-display);
   text-transform: uppercase;
   font-weight: 400;
   font-size: 4.5rem;
