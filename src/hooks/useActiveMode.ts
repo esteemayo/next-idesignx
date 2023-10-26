c3c0f7a133c10d4b97d56e11e2387f9096fe3a0e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-26 05:03:39 +0100

    fix: refactor

--- src/hooks/useActiveMode.ts
+++ src/hooks/useActiveMode.ts
@@ -6,7 +6,7 @@ import { useDarkMode } from './useDarkMode';
 export const useActiveMode = () => {
   const mode = useDarkMode((state) => state.mode);
 
-  const activeMode = useMemo(() => {
+  const activeMode = useMemo((): string => {
     return mode.toString();
   }, [mode]);
 
