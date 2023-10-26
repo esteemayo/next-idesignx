0f1497122853e61aca5e51e37fe3caae7fc2b174
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-26 16:46:25 +0100

    fix: refactor

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -4,9 +4,7 @@ import Image from 'next/image';
 import { FC } from 'react';
 import styled from 'styled-components';
 
-interface ProductImageProps {
-  img: string;
-}
+import { ProductImageProps } from '@/types';
 
 const ProductImage: FC<ProductImageProps> = ({ img }) => {
   return (
