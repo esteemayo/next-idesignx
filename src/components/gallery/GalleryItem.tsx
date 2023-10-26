4034071e768eb2e795ca1147a86c1abe6cc26c99
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-26 16:19:55 +0100

    fix: refactor

--- src/components/gallery/GalleryItem.tsx
+++ src/components/gallery/GalleryItem.tsx
@@ -4,9 +4,7 @@ import Image from 'next/image';
 import { FC } from 'react';
 import styled from 'styled-components';
 
-interface GalleryItemProps {
-  src: string;
-}
+import { GalleryItemProps } from '@/types';
 
 const GalleryItem: FC<GalleryItemProps> = ({ src }) => {
   return (
