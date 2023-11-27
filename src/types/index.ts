4f0d2bbffa8ed8fac866bfa808447c1f916f0351
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-27 20:54:32 +0100

    fix: modify

--- src/types/index.ts
+++ src/types/index.ts
@@ -214,7 +214,7 @@ export interface ModalProps {
   body?: React.ReactElement;
   footer?: React.ReactElement;
   onClose(): void;
-  onSubmit(): void;
+  onSubmit(e?: React.FormEvent<HTMLFormElement>): void;
   secondaryAction?(): void;
 }
 
