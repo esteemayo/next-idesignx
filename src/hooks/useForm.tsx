c739a6a80be0deb5c54fc12a152fcc07a3a6f4e5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-27 20:26:58 +0100

    feat: create useForm react custom hook

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+
+const useForm = (callback: () => void, initialState = {}, validate: any) => {
+  const [errors, setErrors] = useState<any>({});
+  const [formData, setFormData] = useState<object>(initialState);
+
+  const handleChange: React.ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+  > = useCallback(({ target: input }) => {
+    const { name, value } = input;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (Object.values(validate(formData)).length > 0) {
+        setErrors(validate(formData));
+        return;
+      }
+
+      setErrors({});
+      callback();
+      setFormData(initialState);
+    },
+    [callback, formData, initialState, validate]
+  );
+
+  return {
+    errors,
+    formData,
+    handleChange,
+    handleSubmit,
+  };
+};
+
+export default useForm;
