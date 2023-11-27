d94e437dd2b0f35a02c2ee7d2dc2d5a7d1347647
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-27 18:29:45 +0100

    fix: modify

--- src/components/ClientOnly.tsx
+++ src/components/ClientOnly.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ClientOnly = ({ children }: { children: React.ReactNode }) => {
   const [hasMounted, setHasMounted] = useState(false);
