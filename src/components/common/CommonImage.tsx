739b0be3073ae42e4ab52f7f8200b33d1c257dc9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-28 21:35:31 +0100

    feat: create CommonImage component

---
+++ src/components/common/CommonImage.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+
+export const CommonImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+  display: block;
+  object-fit: cover;
+`;
