2072005375d0d8baf0ebaf684b2b716d851bd44e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-29 18:52:44 +0100

    feat: create HomeLayout component

---
+++ src/components/layouts/HomeLayout.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import styled from 'styled-components';
+
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  return <Container>{children}</Container>;
+};
+
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+
+  display: grid;
+  grid-template-rows: 80vh repeat(4, min-content);
+  grid-template-columns:
+    [full-start] minmax(6rem, 1fr) [center-start] repeat(
+      8,
+      [col-start] minmax(min-content, 15rem) [col-end]
+    )
+    [center-end] minmax(6rem, 1fr) [full-end];
+
+  @media only screen and (max-width: 50em) {
+    grid-template-rows: 80vh repeat(5, min-content);
+  }
+
+  @media only screen and (max-width: 37.5em) {
+    grid-template-rows: 60vh;
+  }
+
+  @media only screen and (max-width: 25em) {
+    grid-template-rows: 40vh;
+  }
+`;
+
+export default HomeLayout;
