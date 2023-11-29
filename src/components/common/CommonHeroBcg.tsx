57ed4a25a1ebda91356dff284623ea0185efe4df
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-29 17:47:23 +0100

    fix: modify

--- src/components/common/CommonHeroBcg.tsx
+++ src/components/common/CommonHeroBcg.tsx
@@ -1,17 +1,7 @@
 import styled from 'styled-components';
 
-interface IHeader {
-  bcgImage: string;
-}
-
-export const CommonHeroBcg = styled.header<IHeader>`
+export const CommonHeroBcg = styled.header`
   color: var(--clr-white);
-  background-image: linear-gradient(
-      to right bottom,
-      rgba(0, 0, 0, 0.7),
-      rgba(51, 51, 51, 0.7)
-    ),
-    url(bcgImage);
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
