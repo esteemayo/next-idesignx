36c9c8bf829f22afe5f3864044d710efa662e5ac
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-30 00:58:16 +0100

    fix: refactor

--- src/app/prints/page.tsx
+++ src/app/prints/page.tsx
@@ -1,10 +1,8 @@
-'use client';
-
 import dynamic from 'next/dynamic';
-import styled from 'styled-components';
 
 import { galleryImages } from '@/data';
 import PrintHero from '@/components/hero/PrintHero';
+import PrintsLayout from '@/components/layouts/PrintsLayout';
 
 const Gallery = dynamic(() => import('@/components/gallery/Gallery'), {
   ssr: false,
@@ -15,33 +13,12 @@ const PrintContact = dynamic(() => import('@/components/PrintContact'), {
 
 const Prints = () => {
   return (
-    <Container>
+    <PrintsLayout>
       <PrintHero />
       <Gallery data={galleryImages} />
       <PrintContact />
-    </Container>
+    </PrintsLayout>
   );
 };
-
-const Container = styled.main`
-  background-color: ${({ theme }) => theme.bg};
-
-  display: grid;
-  grid-template-rows: 70vh repeat(2, min-content);
-  grid-template-columns:
-    [full-start] minmax(6rem, 1fr) [center-start]repeat(
-      8,
-      [col-start] minmax(min-content, 15rem) [col-end]
-    )
-    [center-end] minmax(6rem, 1fr) [full-end];
-
-  @media only screen and (max-width: 50em) {
-    grid-template-rows: 60vh;
-  }
-
-  @media only screen and (max-width: 25em) {
-    grid-template-rows: 40vh;
-  }
-`;
 
 export default Prints;
---
+++ src/components/layouts/PrintsLayout.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import styled from 'styled-components';
+
+const PrintsLayout = ({ children }: { children: React.ReactNode }) => {
+  return <Container>{children}</Container>;
+};
+
+const Container = styled.main`
+  background-color: ${({ theme }) => theme.bg};
+
+  display: grid;
+  grid-template-rows: 70vh repeat(2, min-content);
+  grid-template-columns:
+    [full-start] minmax(6rem, 1fr) [center-start]repeat(
+      8,
+      [col-start] minmax(min-content, 15rem) [col-end]
+    )
+    [center-end] minmax(6rem, 1fr) [full-end];
+
+  @media only screen and (max-width: 50em) {
+    grid-template-rows: 60vh;
+  }
+
+  @media only screen and (max-width: 25em) {
+    grid-template-rows: 40vh;
+  }
+`;
+
+export default PrintsLayout
