71c0179c5ff5355f14f5c336526ab093a9a3bc26
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-30 17:51:42 +0100

    feat: add ContactLayout component

---
+++ src/components/layouts/ContactLayout.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import styled from 'styled-components';
+
+const ContactLayout = ({ children }: { children: React.ReactNode }) => {
+  return <Container>{children}</Container>;
+};
+
+const Container = styled.main`
+  background-color: ${({ theme }) => theme.bg};
+
+  display: grid;
+  grid-template-rows: 60vh min-content;
+  grid-template-columns:
+    [full-start] minmax(6rem, 1fr) [center-start] repeat(
+      8,
+      [col-start] minmax(min-content, 15rem) [col-end]
+    )
+    [center-end] minmax(6rem, 1fr) [full-end];
+
+  @media only screen and (max-width: 56.25em) {
+    grid-template-rows: 60vh repeat(2, min-content);
+  }
+
+  @media only screen and (max-width: 50em) {
+    grid-template-rows: 40vh;
+  }
+
+  @media only screen and (max-width: 31.25em) {
+    grid-template-rows: 30vh;
+  }
+`;
+
+export default ContactLayout;
