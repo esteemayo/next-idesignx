f1b4bb927d45e70023bfa40f294ebaad138c34a9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-30 17:59:02 +0100

    fix: refactor

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,16 +3,12 @@ import Location from '@/components/Location';
 import ContactLayout from '@/components/layouts/ContactLayout';
 import ContactForm from '@/components/ContactForm';
 
-import { useActiveMode } from '@/hooks/useActiveMode';
-
 const Contact = () => {
-  const { activeMode } = useActiveMode();
-
   return (
     <ContactLayout>
       <ContactHero />
       <ContactForm />
-      <Location mode={activeMode} />
+      <Location />
     </ContactLayout>
   );
 };
