7a5d667e90c9ba377621c004a648a30a1fabffa3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-30 18:07:52 +0100

    fix: handle seo

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,13 @@
+import type { Metadata } from 'next';
+
 import ContactHero from '@/components/hero/ContactHero';
 import Location from '@/components/Location';
 import ContactLayout from '@/components/layouts/ContactLayout';
 import ContactForm from '@/components/ContactForm';
+
+export const metadata: Metadata = {
+  title: 'iDesignx | Contact',
+};
 
 const Contact = () => {
   return (
