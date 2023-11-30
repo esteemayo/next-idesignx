c3d7d50db9550cc552b00c7e4ed0a30f8581f567
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-30 01:23:37 +0100

    fix: refactor

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,7 @@
-'use client';
-
 import dynamic from 'next/dynamic';
-import styled from 'styled-components';
 
 import GalleryHero from '@/components/hero/GalleryHero';
+import GalleryLayout from '@/components/layouts/GalleryLayout';
 
 const PrintContact = dynamic(() => import('@/components/PrintContact'), {
   ssr: false,
@@ -17,33 +15,12 @@ const ImageGallery = dynamic(
 
 const Gallery = () => {
   return (
-    <Container>
+    <GalleryLayout>
       <GalleryHero />
       <ImageGallery />
       <PrintContact bcg='/img/gallery/gal-15.jpg' />
-    </Container>
+    </GalleryLayout>
   );
 };
-
-const Container = styled.main`
-  background-color: ${({ theme }) => theme.bg};
-
-  display: grid;
-  grid-template-rows: 70vh repeat(2, min-content);
-  grid-template-columns:
-    [full-start] minmax(6rem, 1fr) [center-start] repeat(
-      8,
-      [col-start] minmax(min-content, 15rem) [col-end]
-    )
-    [center-end] minmax(6rem, 1fr) [full-end];
-
-  @media only screen and (max-width: 37.5em) {
-    grid-template-rows: 60vh;
-  }
-
-  @media only screen and (max-width: 25em) {
-    grid-template-rows: 40vh;
-  }
-`;
 
 export default Gallery;
