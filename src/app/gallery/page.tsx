232a6dbe7ad4f1fd3c168bce422b0a900f44fdaa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-30 18:11:15 +0100

    fix: handle seo

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,15 @@
+import type { Metadata } from 'next';
+
 import GalleryHero from '@/components/hero/GalleryHero';
 import GalleryLayout from '@/components/layouts/GalleryLayout';
 import PrintContact from '@/components/PrintContact';
 import ImageGallery from '@/components/gallery/ImageGallery';
 
 import { galleryItems } from '@/data';
+
+export const metadata: Metadata = {
+  title: 'iDesignx | Gallery',
+};
 
 const Gallery = () => {
   return (
