6f6452973f1f3d7d6ec55b43b8c4701f9292358e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-30 01:43:33 +0100

    fix: modify

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,13 @@
-import dynamic from 'next/dynamic';
+import AboutHero from '@/components/hero/AboutHero';
+import AboutLayout from '@/components/layouts/AboutLayout';
+import StoryImage from '@/components/StoryImage';
+import StoryDetails from '@/components/StoryDetails';
+import StoryCards from '@/components/StoryCards';
 
 import Photo from './Photo';
 import Contact from './Contact';
 
 import { cardItems, storyImages } from '@/data';
-import AboutHero from '@/components/hero/AboutHero';
-import AboutLayout from '@/components/layouts/AboutLayout';
-
-const StoryImage = dynamic(() => import('@/components/StoryImage'), {
-  ssr: false,
-});
-const StoryDetails = dynamic(() => import('@/components/StoryDetails'), {
-  ssr: false,
-});
-const StoryCards = dynamic(() => import('@/components/StoryCards'), {
-  ssr: false,
-});
 
 const About = () => {
   return (
