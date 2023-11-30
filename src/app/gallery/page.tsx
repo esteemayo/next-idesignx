66a26aebfb823dbe4d94275de3485f5470ad6a9f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-30 01:32:33 +0100

    fix: pass data via props

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -3,11 +3,13 @@ import GalleryLayout from '@/components/layouts/GalleryLayout';
 import PrintContact from '@/components/PrintContact';
 import ImageGallery from '@/components/gallery/ImageGallery';
 
+import { galleryItems } from '@/data';
+
 const Gallery = () => {
   return (
     <GalleryLayout>
       <GalleryHero />
-      <ImageGallery />
+      <ImageGallery galleryImages={galleryItems} />
       <PrintContact bcg='/img/gallery/gal-15.jpg' />
     </GalleryLayout>
   );
