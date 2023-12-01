7d24c47eeea58c6d349554863ca1800b71484c5b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-01 01:07:36 +0100

    fix: handle seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from 'next';
+
 import About from '@/components/About';
 import Contact from '@/components/Contact';
 import Hero from '@/components/hero/Hero';
@@ -7,6 +9,10 @@ import Printings from '@/components/Printings';
 import Products from '@/components/Products';
 
 import { brands, printItems, productImages } from '@/data';
+
+export const metadata: Metadata = {
+  title: 'iDesignx | Home',
+};
 
 const Home = () => {
   return (
