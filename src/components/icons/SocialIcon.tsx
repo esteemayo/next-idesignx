c9b919b4e27059521ff275a12e06eed6b88e785f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-01 01:50:51 +0100

    fix: refactor

--- src/components/icons/SocialIcon.tsx
+++ src/components/icons/SocialIcon.tsx
@@ -1,27 +1,47 @@
 'use client';
 
+import Link from 'next/link';
 import styled, { css } from 'styled-components';
 
-import { IconLink } from '../buttons/IconLink';
+import { useActiveMode } from '@/hooks/useActiveMode';
 
 interface SocialIconProps {
   icon: string;
+}
+
+interface IMode {
   mode: string;
 }
 
-interface ISpan {
-  icon: string;
-}
+const SocialIcon = ({ icon }: SocialIconProps) => {
+  const { activeMode } = useActiveMode();
 
-const SocialIcon = ({ icon, mode }: SocialIconProps) => {
   return (
-    <IconLink href='#' mode={mode}>
+    <Container href='#' mode={activeMode}>
       <Icon icon={icon}>&nbsp;</Icon>
-    </IconLink>
+    </Container>
   );
 };
 
-const Icon = styled.span<ISpan>`
+const Container = styled(Link)<IMode>`
+  width: 4rem;
+  height: 4rem;
+  text-decoration: none;
+  border-radius: 50%;
+  outline-color: ${({ theme }) => theme.outline};
+  transition: all 0.2s;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: ${({ mode }) =>
+      mode === 'true' ? '#2b3138' : '#f8e5fc'};
+  }
+`;
+
+const Icon = styled.span<SocialIconProps>`
   display: inline-block;
   width: 2rem;
   height: 2rem;
@@ -29,15 +49,16 @@ const Icon = styled.span<ISpan>`
   -webkit-mask-image: ${({ icon }) => setMaskImg(icon)};
   mask-image: ${({ icon }) => setMaskImg(icon)};
   background-image: ${({ icon }) =>
-    icon === 'instagram' &&
-    css`
+    icon === 'instagram'
+      ? css`
   linear-gradient(
     to bottom left,
     var(--clr-instagram-1),
     var(--clr-instagram-2),
     var(--clr-instagram-3)
   )
-  `};
+  `
+      : undefined};
   -webkit-mask-size: cover;
   mask-size: cover;
 `;
