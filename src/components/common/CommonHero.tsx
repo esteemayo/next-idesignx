67c0cafa4c50193587e55cedb288eaf35c1c752d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-02 15:43:43 +0100

    fix: add css styling

--- src/components/common/CommonHero.tsx
+++ src/components/common/CommonHero.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const CommonHero = styled.header`
+  background-color: #ddd;
   color: var(--clr-white);
   background-size: cover;
   background-position: center;
