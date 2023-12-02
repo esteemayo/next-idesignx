41182ce2f10ba4440ed9595996ba0be98ad30432
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-02 16:04:22 +0100

    fix: add css styling

--- src/components/StoryImage.tsx
+++ src/components/StoryImage.tsx
@@ -26,6 +26,7 @@ const StoryImage = ({ images }: StoryImageProps) => {
 };
 
 const StyledImage = styled(CommonImage)`
+  background-color: var(--clr-gray-light-3);
   box-shadow: 0 0 4rem rgba(0, 0, 0, 0.09);
   z-index: 1000;
   outline-offset: 2rem;
