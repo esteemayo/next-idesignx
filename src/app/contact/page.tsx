f4cb6cbedfbcc8dae45904b1c63acee80a545a41
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-03 02:19:52 +0100

    fix: modify

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 
 import ContactHero from '@/components/hero/ContactHero';
 import ContactInfo from '@/components/contacts/ContactInfo';
-import ContactLayout from '@/components/layouts/ContactLayout';
 import ContactForm from '@/components/ContactForm';
+import ContactLayout from '@/components/layouts/ContactLayout';
 
 export const metadata: Metadata = {
   title: 'iDesignx | Contact',
