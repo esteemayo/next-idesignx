db88ebfdfb4218fd5ace4cf01ba5333df336140b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-03 20:41:23 +0100

    fix: change file directory

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 
 import AboutHero from '@/components/hero/AboutHero';
 import AboutLayout from '@/components/layouts/AboutLayout';
-import StoryImage from '@/components/StoryImage';
+import StoryImage from '@/components/story/StoryImage';
 import StoryCards from '@/components/story/StoryCards';
 import StoryDetails from '@/components/story/StoryDetails';
 
