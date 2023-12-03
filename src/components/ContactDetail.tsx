9800aac0f55c38b49e270752b8e12d0d5121b39b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-03 01:47:27 +0100

    feat: add markup

--- src/components/ContactDetail.tsx
+++ src/components/ContactDetail.tsx
@@ -2,8 +2,56 @@
 
 import styled from 'styled-components';
 
-const ContactDetail = () => {
-  return <div>ContactDetail</div>;
+const ContactDetail = ({
+  title,
+  address,
+  state,
+  email1,
+  email2,
+  phone1,
+  phone2,
+}) => {
+  return (
+    <Container>
+      <Heading>{title}</Heading>
+      {address && (
+        <Text>
+          {address}
+          <Br />
+          {state}
+        </Text>
+      )}
+      {email1 && <Text>{email1}</Text>}
+      {email2 && <Text>{email2}</Text>}
+      {phone1 && <Text>{phone1}</Text>}
+      {phone2 && <Text>{phone2}</Text>}
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  margin-bottom: 3rem;
+`;
+
+const Heading = styled.h3`
+  text-transform: capitalize;
+  font-weight: lighter;
+  font-size: 2rem;
+  margin-bottom: 1rem;
+
+  @media only screen and (max-width: 56.25em) {
+    font-size: 1.85rem;
+  }
+`;
+
+const Text = styled.p`
+  font-size: 1.8rem;
+
+  @media only screen and (max-width: 56.25em) {
+    font-size: 1.65rem;
+  }
+`;
+
+const Br = styled.br``;
 
 export default ContactDetail;
