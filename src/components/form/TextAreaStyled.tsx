fe870d6a25703adaad5193d700ef0ac49ba91d31
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-04 01:39:58 +0100

    fix: change type annotation

--- src/components/form/TextAreaStyled.tsx
+++ src/components/form/TextAreaStyled.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import styled from 'styled-components';
+
 import { CommonTextArea } from '../common/CommonTextArea';
 
 interface ITextArea {
-  error?: string | undefined;
+  error?: string;
 }
 
 export const TextAreaStyled = styled(CommonTextArea)<ITextArea>`
@@ -17,18 +20,18 @@ export const TextAreaStyled = styled(CommonTextArea)<ITextArea>`
   }
 `;
 
-const setBorderColor = (error: string | undefined) => {
+const setBorderColor = (error?: string) => {
   return error ? 'var(--clr-red)' : '#aaa';
 };
 
-const setOutlineColor = (error: string | undefined) => {
+const setOutlineColor = (error?: string) => {
   return error ? 'var(--clr-red)' : '#979797';
 };
 
-const setCaretColor = (error: string | undefined) => {
+const setCaretColor = (error?: string) => {
   return error ? 'var(--clr-red)' : 'var(--clr-purple-light-3)';
 };
 
-const setPlaceholderColor = (error: string | undefined) => {
+const setPlaceholderColor = (error?: string) => {
   return error ? 'var(--clr-red)' : '#bbb';
 };
