fecdf939f6cfdfa413bda7ef029174cb21df14da
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-04 01:56:45 +0100

    feat: add dark/light mode styling

--- src/app/about/Photo.tsx
+++ src/app/about/Photo.tsx
@@ -9,7 +9,7 @@ const Photo = () => {
 const Container = styled.section`
   grid-column: full-start / full-end;
 
-  background-color: var(--clr-gray-light-3);
+  background-color: ${({ theme }) => theme.bgImg};
   background-image: url(/img/about-bg.jpg);
   background-size: cover;
   background-position: center;
