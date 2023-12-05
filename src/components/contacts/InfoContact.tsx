edb3b4e0f9f420256c1d1877db47a54fc2e07626
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-05 02:23:16 +0100

    feat: add markup

--- src/components/contacts/InfoContact.tsx
+++ src/components/contacts/InfoContact.tsx
@@ -1,9 +1,23 @@
 'use client';
 
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const InfoContact = () => {
-  return <div>InfoContact</div>;
+const InfoContact = ({ icon, text }) => {
+  return (
+    <PhoneWrapper>
+      <FontAwesomeIcon icon={icon} />
+      <PhoneNumber>{text}</PhoneNumber>
+    </PhoneWrapper>
+  );
 };
+
+const PhoneWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const PhoneNumber = styled.span``;
 
 export default InfoContact;
