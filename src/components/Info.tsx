5b41be4551dc2922b36391eca5ab119f0d39e80e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-05 02:14:54 +0100

    fix: refactor

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -105,63 +105,6 @@ const SocialWrapper = styled.div`
   gap: 0.5rem;
 `;
 
-const StyledLink = styled(Link)<IMode>`
-  width: 3rem;
-  height: 3rem;
-  text-decoration: none;
-  background-color: transparent;
-  color: currentColor;
-  outline-color: ${({ theme }) => theme.outline};
-  border-radius: 50%;
-  transition: all 0.2s;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &:hover,
-  &:active {
-    background-color: ${({ mode }) =>
-      mode === 'true' ? '#2b3138' : '#e5bffd'};
-    color: currentColor;
-  }
-
-  &:first-child {
-    color: ${({ mode }) =>
-      setColor(mode, 'currentColor', 'var(--clr-facebook)')};
-
-    &:hover {
-      color: ${({ mode }) =>
-        setColor(mode, 'var(--clr-facebook)', 'currentColor')};
-    }
-  }
-
-  &:nth-child(2) {
-    color: ${({ mode }) =>
-      setColor(mode, 'currentColor', 'var(--clr-twitter)')};
-
-    &:hover {
-      color: ${({ mode }) =>
-        setColor(mode, 'var(--clr-twitter)', 'currentColor')};
-    }
-  }
-
-  &:last-child {
-    color: ${({ mode }) =>
-      setColor(mode, 'currentColor', 'var(--clr-instagram-2)')};
-
-    &:hover {
-      color: ${({ mode }) =>
-        setColor(mode, 'var(--clr-instagram-2)', 'currentColor')};
-    }
-  }
-
-  svg {
-    font-size: 2rem;
-    fill: currentColor;
-  }
-`;
-
 const setColor = (mode: string, color1: string, color2: string): string => {
   return mode !== 'true' ? color1 : color2;
 };
