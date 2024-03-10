fd67851ec95c3bc84ede8bd015bec97bea376b8a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-10 02:15:45 +0100

    fix: modify

--- src/app/designs/page.tsx
+++ src/app/designs/page.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next';
 
-import DesignHero from '@/components/hero/DesignHero';
-import Mission from '@/components/Mission';
-import DesignsLayout from '@/components/layouts/DesignsLayout';
-import Request from '@/components/Request';
 import Innovation from '@/components/Innovation';
+import Mission from '@/components/Mission';
+import DesignHero from '@/components/hero/DesignHero';
+import Request from '@/components/Request';
+import DesignsLayout from '@/components/layouts/DesignsLayout';
 import Statement from '@/components/Statement';
 
 // import Featured from '@/components/Featured';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next';
 
 import About from '@/components/About';
+import Printings from '@/components/Printings';
+import SeenOn from '@/components/SeenOn';
 import Contact from '@/components/contacts/Contact';
 import Hero from '@/components/hero/Hero';
 import HomeLayout from '@/components/layouts/HomeLayout';
-import SeenOn from '@/components/SeenOn';
-import Printings from '@/components/Printings';
 import Products from '@/components/Products';
 
 import { brands, printItems, productImages } from '@/data';
