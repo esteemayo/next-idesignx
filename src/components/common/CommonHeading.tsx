f187387717f48591419987e540f923cd5bb4479e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-29 18:13:38 +0100

    feat: add media query

--- src/components/common/CommonHeading.tsx
+++ src/components/common/CommonHeading.tsx
@@ -8,4 +8,12 @@ export const CommonHeading = styled.h1`
   font-weight: 400;
   font-size: 4.5rem;
   line-height: 1;
+
+  @media only screen and (min-width: 112.5em) {
+    font-size: 4.7rem;
+  }
+
+  @media only screen and (min-width: 125em) {
+    font-size: 5rem;
+  }
 `;
