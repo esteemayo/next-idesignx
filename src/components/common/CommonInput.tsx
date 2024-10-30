691cb8d8eb4530c83775fec05b6bd53625c3ae16
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-30 17:49:48 +0100

    feat: add styling

--- src/components/common/CommonInput.tsx
+++ src/components/common/CommonInput.tsx
@@ -12,6 +12,22 @@ export const CommonInput = styled.input`
   border-radius: 2px;
   transition: all 0.2s;
 
+  @media only screen and (max-width: 56.25em) {
+    font-size: var(--default-font-size);
+  }
+
+  @media only screen and (max-width: 50em) {
+    font-size: 1.7rem;
+  }
+
+  @media only screen and (max-width: 34.375em) {
+    font-size: 1.8rem;
+  }
+
+  @media only screen and (max-width: 25em) {
+    font-size: var(--default-font-size);
+  }
+
   @media only screen and (min-width: 112.5em) {
     font-size: 1.7rem;
     padding: 2rem;
