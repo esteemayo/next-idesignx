87270b33ab432636531e9d8280625120cf8efdc2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-30 17:52:42 +0100

    feat: add styling

--- src/components/common/CommonSelect.tsx
+++ src/components/common/CommonSelect.tsx
@@ -10,6 +10,22 @@ export const CommonSelect = styled.select`
   padding: 1.5rem 2rem;
   border-radius: 2px;
 
+  @media only screen and (max-width: 56.25em) {
+    font-size: var(--default-font-size);
+  }
+
+  @media only screen and (max-width: 50em) {
+    font-size: 1.7rem;
+  }
+
+  @media only screen and (max-width: 34.375em) {
+    font-size: 1.8rem;
+  }
+
+  @media only screen and (max-width: 25em) {
+    font-size: var(--default-font-size);
+  }
+
   @media only screen and (min-width: 112.5em) {
     font-size: 1.7rem;
     padding: 2rem;
