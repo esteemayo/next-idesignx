3c4a0dc12068fb91857789243dcff2c6199590de
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-30 17:53:50 +0100

    feat: add media query

--- src/components/form/StyledLabel.tsx
+++ src/components/form/StyledLabel.tsx
@@ -22,6 +22,22 @@ export const StyledLabel = styled.label<ILabel>`
   color: ${({ mode, error }) => setColor(mode, error)};
   margin-bottom: 1rem;
 
+  @media only screen and (max-width: 56.25em) {
+    font-size: 1.5rem;
+  }
+
+  @media only screen and (max-width: 50em) {
+    font-size: var(--default-font-size);
+  }
+
+  @media only screen and (max-width: 34.375em) {
+    font-size: 1.7rem;
+  }
+
+  @media only screen and (max-width: 25em) {
+    font-size: 1.5rem;
+  }
+
   @media only screen and (min-width: 112.5em) {
     font-size: var(--default-font-size);
   }
