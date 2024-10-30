a2aaf4b7872629038ce90b75563e9bfdcff3c9bb
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-30 17:51:07 +0100

    feat: add styling

--- src/components/form/StyledInput.tsx
+++ src/components/form/StyledInput.tsx
@@ -20,6 +20,22 @@ export const StyledInput = styled(CommonInput)<IInput>`
     font-size: 1.4rem;
     color: var(--clr-gray-dark-2);
 
+    @media only screen and (max-width: 56.25em) {
+      font-size: 1.5rem;
+    }
+
+    @media only screen and (max-width: 50em) {
+      font-size: var(--default-font-size);
+    }
+
+    @media only screen and (max-width: 34.375em) {
+      font-size: 1.7rem;
+    }
+
+    @media only screen and (max-width: 25em) {
+      font-size: 1.5rem;
+    }
+
     @media only screen and (min-width: 112.5em) {
       font-size: var(--default-font-size);
     }
