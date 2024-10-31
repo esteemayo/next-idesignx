a2aafd9787f149806f3c5034ff8cc6122d744247
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-31 16:15:03 +0100

    feat: add styling

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -44,6 +44,10 @@ const HeadingPrimary = styled.h1`
     font-size: 2.25rem;
   }
 
+  @media only screen and (max-width: 18.75em) {
+    font-size: 2rem;
+  }
+
   @media only screen and (min-width: 106.25em) {
     font-size: 2.6rem;
   }
--- src/components/form/StyledLabel.tsx
+++ src/components/form/StyledLabel.tsx
@@ -38,6 +38,10 @@ export const StyledLabel = styled.label<ILabel>`
     font-size: 1.5rem;
   }
 
+  @media only screen and (max-width: 18.75em) {
+    width: 35%;
+  }
+
   @media only screen and (min-width: 106.25em) {
     font-size: 1.5rem;
   }
