2b4551cd5a0352bebe450293b19e515632757285
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-31 19:39:54 +0100

    fix: modify

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -137,7 +137,7 @@ const Text = styled.p`
   }
 
   @media only screen and (max-width: 20em) {
-    font-size: 1.3rem;
+    font-size: 1.5rem;
   }
 `;
 
@@ -172,7 +172,7 @@ const Paragraph = styled.p`
   }
 
   @media only screen and (max-width: 20em) {
-    font-size: 1.3rem;
+    font-size: 1.5rem;
   }
 `;
 
