cfc7882c042ae5c82c971a9385d9bb3008094abb
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-01 01:05:11 +0100

    feat: add styling

--- src/components/Statement.tsx
+++ src/components/Statement.tsx
@@ -26,6 +26,18 @@ const Container = styled.section`
   @media only screen and (max-width: 75em) {
     padding: 3rem 0;
   }
+
+  @media only screen and (min-width: 106.25em) {
+    padding: 4.5rem;
+  }
+
+  @media only screen and (min-width: 112.5em) {
+    padding: 5rem;
+  }
+
+  @media only screen and (min-width: 125em) {
+    padding: 6rem;
+  }
 `;
 
 const Paragraph = styled.p`
@@ -36,6 +48,18 @@ const Paragraph = styled.p`
   @media only screen and (max-width: 25em) {
     font-size: 1.85rem;
     margin-bottom: 1.7rem;
+  }
+
+  @media only screen and (min-width: 106.25em) {
+    font-size: 2.25rem;
+  }
+
+  @media only screen and (min-width: 112.5em) {
+    font-size: 2.5rem;
+  }
+
+  @media only screen and (min-width: 125em) {
+    font-size: 3rem;
   }
 `;
 
