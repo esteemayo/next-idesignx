fc3d88ee5c02ab2354461ddfb552c9e5a8ba87d1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-03 04:08:28 +0100

    feat: add styling

--- src/app/about/Contact.tsx
+++ src/app/about/Contact.tsx
@@ -47,19 +47,31 @@ const Text = styled.p`
   line-height: 1;
 
   @media only screen and (max-width: 56.25em) {
-    font-size: 3.2rem;
-  }
-
-  @media only screen and (max-width: 50em) {
     font-size: 3.3rem;
   }
 
-  @media only screen and (max-width: 34.375em) {
+  @media only screen and (max-width: 50em) {
     font-size: 3.4rem;
   }
 
+  @media only screen and (max-width: 43.75em) {
+    font-size: 3rem;
+  }
+
+  @media only screen and (max-width: 40.625em) {
+    font-size: 2.8rem;
+  }
+
+  @media only screen and (max-width: 37.5em) {
+    font-size: 2.5rem;
+  }
+
+  @media only screen and (max-width: 34.375em) {
+    font-size: 2.25rem;
+  }
+
   @media only screen and (max-width: 25em) {
-    font-size: 3.2rem;
+    font-size: 3.3rem;
   }
 
   @media only screen and (min-width: 106.25em) {
