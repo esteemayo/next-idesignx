a8033c2c04501652e4f2bc365a66f4b5ed083d22
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-04 16:33:39 +0100

    fix: modify

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next';
 
+import StoryCards from '@/components/story/StoryCards';
 import AboutHero from '@/components/hero/AboutHero';
 import AboutLayout from '@/components/layouts/AboutLayout';
 import StoryImage from '@/components/story/StoryImage';
 import StoryDetails from '@/components/story/StoryDetails';
-import StoryCards from '@/components/story/StoryCards';
 
 import Photo from './Photo';
 import Contact from './Contact';
