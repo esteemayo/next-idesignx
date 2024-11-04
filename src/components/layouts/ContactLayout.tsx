9222fef5a1051985e70e40de701f147faef2c343
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-04 21:55:34 +0100

    feat: add media query

--- src/components/layouts/ContactLayout.tsx
+++ src/components/layouts/ContactLayout.tsx
@@ -10,13 +10,17 @@ const Container = styled.main`
   background-color: ${({ theme }) => theme.bg};
 
   display: grid;
-  grid-template-rows: 60vh min-content;
+  grid-template-rows: 32vw min-content;
   grid-template-columns:
     [full-start] minmax(6rem, 1fr) [center-start] repeat(
       8,
       [col-start] minmax(min-content, 15rem) [col-end]
     )
     [center-end] minmax(6rem, 1fr) [full-end];
+
+  @media only screen and (max-width: 64em) {
+    grid-template-rows: 35vw min-content;
+  }
 
   @media only screen and (max-width: 56.25em) {
     grid-template-rows: 60vh repeat(2, min-content);
