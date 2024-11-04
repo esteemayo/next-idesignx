47bcfc4956ece00bf2ea8ef73bceb5cea284f95d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-04 18:20:11 +0100

    feat: add styling

--- src/components/hero/AboutHero.tsx
+++ src/components/hero/AboutHero.tsx
@@ -104,6 +104,10 @@ const Text = styled.p`
     font-size: 1.2rem;
   }
 
+  @media only screen and (max-width: 17.5em) {
+    font-size: 1.15rem;
+  }
+
   @media only screen and (min-width: 106.25em) {
     font-size: 2.6rem;
   }
