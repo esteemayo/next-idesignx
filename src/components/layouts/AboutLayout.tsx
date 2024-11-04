af0dfd398484714b1497fd55009281d894d53454
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-04 16:29:42 +0100

    feat: add media query

--- src/components/layouts/AboutLayout.tsx
+++ src/components/layouts/AboutLayout.tsx
@@ -38,8 +38,12 @@ const Container = styled.main`
     grid-template-rows: 40vh min-content 30vw min-content 35vw;
   }
 
+  @media only screen and (max-width: 26.875em) {
+    grid-template-rows: 40vh min-content 30vw min-content 40vw;
+  }
+
   @media only screen and (max-width: 25em) {
-    grid-template-rows: 30vh min-content 30vw min-content 20vw;
+    grid-template-rows: 30vh min-content 30vw min-content 43vw;
   }
 `;
 
