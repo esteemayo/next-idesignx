2eb560397645de90636f7fdd7c10156ff4137385
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-04 18:24:30 +0100

    feat: add styling

--- src/components/layouts/AboutLayout.tsx
+++ src/components/layouts/AboutLayout.tsx
@@ -10,7 +10,7 @@ const Container = styled.main`
   background-color: ${({ theme }) => theme.bg};
 
   display: grid;
-  grid-template-rows: 60vh 40vw min-content 20vw min-content;
+  grid-template-rows: 32vw 40vw min-content 20vw min-content;
   grid-template-columns:
     [full-start]minmax(6rem, 1fr) [center-start] repeat(
       8,
@@ -18,12 +18,16 @@ const Container = styled.main`
     )
     [center-end] minmax(6rem, 1fr) [full-end];
 
+  @media only screen and (max-width: 64em) {
+    grid-template-rows: 35vw min-content 30vw min-content 23vw;
+  }
+
   @media only screen and (max-width: 56.25em) {
-    grid-template-rows: 60vh min-content 30vw min-content 23vw min-content;
+    grid-template-rows: 35vw min-content 30vw min-content 23vw;
   }
 
   @media only screen and (max-width: 50em) {
-    grid-template-rows: 60vh min-content 30vw min-content 25vw min-content;
+    grid-template-rows: 35vw min-content 30vw min-content 25vw;
   }
 
   @media only screen and (max-width: 37.5em) {
