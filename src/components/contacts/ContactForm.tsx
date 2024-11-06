b4000fc2b671aa13e7b964af54deade7778f1688
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-06 15:27:00 +0100

    feat: add media query

--- src/components/contacts/ContactForm.tsx
+++ src/components/contacts/ContactForm.tsx
@@ -206,6 +206,10 @@ const Heading = styled.h3`
     font-size: var(--default-font-size);
   }
 
+  @media only screen and (max-width: 17.5em) {
+    font-size: 1.5rem;
+  }
+
   @media only screen and (min-width: 106.25em) {
     font-size: 2.6rem;
   }
