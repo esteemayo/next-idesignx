8fa892ed3ab36dd220785c10da8068106f1caf32
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-07 19:52:43 +0100

    feat: add media query

--- src/components/contacts/InfoContact.tsx
+++ src/components/contacts/InfoContact.tsx
@@ -67,6 +67,38 @@ const Container = styled.div`
 
 const Details = styled.span`
   font-size: 1.4rem;
+
+  @media only screen and (max-width: 56.25em) {
+    font-size: 1.5rem;
+  }
+
+  @media only screen and (max-width: 50em) {
+    font-size: var(--default-font-size);
+  }
+
+  @media only screen and (max-width: 34.375em) {
+    font-size: 1.7rem;
+  }
+
+  @media only screen and (max-width: 25em) {
+    font-size: 1.5rem;
+  }
+
+  @media only screen and (max-width: 21.5em) {
+    font-size: 1.23rem;
+  }
+
+  @media only screen and (max-width: 18.75em) {
+    font-size: 1.2rem;
+  }
+
+  @media only screen and (min-width: 112.5em) {
+    font-size: var(--default-font-size);
+  }
+
+  @media only screen and (min-width: 125em) {
+    font-size: 1.8rem;
+  }
 `;
 
 export default InfoContact;
