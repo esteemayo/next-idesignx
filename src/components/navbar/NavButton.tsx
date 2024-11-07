4e3bbfd75b0dcd4798b90cd902004a1f89caf35d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-07 17:49:53 +0100

    feat: add media query

--- src/components/navbar/NavButton.tsx
+++ src/components/navbar/NavButton.tsx
@@ -19,6 +19,14 @@ const Button = styled.button`
   cursor: pointer;
   transition: all 0.2s;
 
+  @media only screen and (max-width: 56.25em) {
+    font-size: 1.7rem;
+  }
+
+  @media only screen and (max-width: 50em) {
+    font-size: 1.8rem;
+  }
+
   @media only screen and (min-width: 112.5em) {
     font-size: 1.8rem;
   }
