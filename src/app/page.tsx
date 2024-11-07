7cb75b8a120b48b71da2d4b5409892b90996714a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-07 17:43:10 +0100

    fix: modify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next';
 
+import Products from '@/components/Products';
 import About from '@/components/About';
 import Printings from '@/components/Printings';
 import SeenOn from '@/components/SeenOn';
 import Contact from '@/components/contacts/Contact';
 import Hero from '@/components/hero/Hero';
 import HomeLayout from '@/components/layouts/HomeLayout';
-import Products from '@/components/Products';
 
 import { brands, printItems, productImages } from '@/data';
 
