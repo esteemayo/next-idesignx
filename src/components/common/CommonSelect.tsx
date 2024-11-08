318bb5e6ff67fcd865ddec41710815eb54a70980
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-08 01:21:19 +0100

    feat: add media query

--- src/components/common/CommonSelect.tsx
+++ src/components/common/CommonSelect.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 export const CommonSelect = styled.select`
   width: 100%;
+  height: 5.8rem;
   display: inline-block;
   font-family: inherit;
   font-size: 1.5rem;
@@ -26,12 +27,20 @@ export const CommonSelect = styled.select`
     font-size: var(--default-font-size);
   }
 
+  @media only screen and (min-width: 106.25em) {
+    height: 5.9rem;
+    font-size: var(--default-font-size);
+    padding: 2rem;
+  }
+
   @media only screen and (min-width: 112.5em) {
+    height: 6rem;
     font-size: 1.7rem;
     padding: 2rem;
   }
 
   @media only screen and (min-width: 125em) {
+    height: 6.1rem;
     font-size: 2rem;
     padding: 2.25rem 2rem;
   }
