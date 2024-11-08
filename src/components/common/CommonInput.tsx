d7270614ab1e42f6822d64f6fdf38ba6fa13a491
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-09 00:53:21 +0100

    feat: add media query

--- src/components/common/CommonInput.tsx
+++ src/components/common/CommonInput.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 export const CommonInput = styled.input`
   width: 100%;
+  height: 5.8rem;
   display: inline-block;
   font-family: inherit;
   font-size: 1.5rem;
@@ -13,32 +14,43 @@ export const CommonInput = styled.input`
   transition: all 0.2s;
 
   @media only screen and (max-width: 56.25em) {
+    height: 6rem;
     font-size: var(--default-font-size);
   }
 
   @media only screen and (max-width: 50em) {
+    height: 6.2rem;
     font-size: 1.7rem;
   }
 
   @media only screen and (max-width: 34.375em) {
+    height: 6.4rem;
     font-size: 1.8rem;
   }
 
   @media only screen and (max-width: 25em) {
+    height: 6rem;
     font-size: var(--default-font-size);
   }
 
+  @media only screen and (max-width: 21.5em) {
+    height: 5.5rem;
+  }
+
   @media only screen and (min-width: 106.25em) {
+    height: 6rem;
     font-size: var(--default-font-size);
     padding: 1.75rem 2rem;
   }
 
   @media only screen and (min-width: 112.5em) {
+    height: 6.7rem;
     font-size: 1.7rem;
     padding: 2rem;
   }
 
   @media only screen and (min-width: 125em) {
+    height: 7.5rem;
     font-size: 2rem;
     padding: 2.25rem 2rem;
   }
