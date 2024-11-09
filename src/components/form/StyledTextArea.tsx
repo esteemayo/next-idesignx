4116fb4a999a70a70573a241749a5ee24aedfbc7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-09 01:10:39 +0100

    feat: add styling

--- src/components/form/StyledTextArea.tsx
+++ src/components/form/StyledTextArea.tsx
@@ -36,6 +36,10 @@ export const StyledTextArea = styled(CommonTextArea)<ITextArea>`
       font-size: 1.5rem;
     }
 
+    @media only screen and (min-width: 106.25em) {
+      font-size: 1.5rem;
+    }
+
     @media only screen and (min-width: 112.5em) {
       font-size: var(--default-font-size);
     }
