0da20eba49f994f57eabea211de169674455f402
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-10 19:47:11 +0100

    feat: add styling

--- src/components/hero/PrintHero.tsx
+++ src/components/hero/PrintHero.tsx
@@ -161,6 +161,18 @@ const HeadingMain = styled.span`
     font-size: 1.35rem;
     letter-spacing: 1px;
   }
+
+  @media only screen and (min-width: 106.25em) {
+    font-size: 2.5rem;
+  }
+
+  @media only screen and (min-width: 112.5em) {
+    font-size: 2.8rem;
+  }
+
+  @media only screen and (min-width: 125em) {
+    font-size: 3.1rem;
+  }
 `;
 
 export default PrintHero;
