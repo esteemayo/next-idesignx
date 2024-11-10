16549e8f7b67339ad2384a51fbc17e7cf73d2124
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-10 14:52:50 +0100

    feat: add media query

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -113,6 +113,22 @@ const Button = styled.button`
   border-radius: 10rem;
   cursor: pointer;
 
+  @media only screen and (max-width: 56.25em) {
+    font-size: 1.5rem;
+  }
+
+  @media only screen and (max-width: 50em) {
+    font-size: var(--default-font-size);
+  }
+
+  @media only screen and (max-width: 34.375em) {
+    font-size: 1.7rem;
+  }
+
+  @media only screen and (max-width: 25em) {
+    font-size: 1.5rem;
+  }
+
   @media only screen and (min-width: 106.25em) {
     width: 21rem;
     height: 6.5rem;
