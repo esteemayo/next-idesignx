8588e3d6f486012783e67df524ca16bf80e67873
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-11 01:20:55 +0100

    feat: add styling

--- src/components/layouts/DesignsLayout.tsx
+++ src/components/layouts/DesignsLayout.tsx
@@ -9,13 +9,17 @@ const DesignsLayout = ({ children }: { children: React.ReactNode }) => {
 const Container = styled.main`
   background-color: ${({ theme }) => theme.bgDesign};
   display: grid;
-  grid-template-rows: 42vw 40vw repeat(2, min-content);
+  grid-template-rows: 37vw 40vw repeat(2, min-content);
   grid-template-columns:
     [full-start] minmax(6rem, 1fr) [center-start] repeat(
       8,
       [col-start] minmax(min-content, 15rem) [col-end]
     )
     [center-end] minmax(6rem, 1fr) [full-end];
+
+  @media only screen and (max-width: 64em) {
+    grid-template-rows: 43vw 40vw repeat(2, min-content);
+  }
 
   @media only screen and (max-width: 56.25em) {
     grid-template-rows: 47vw 45vw repeat(2, min-content);
