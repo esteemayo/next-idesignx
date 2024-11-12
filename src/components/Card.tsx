3bbd3bf52d3c2d33fe8be86a22f6e86290e533bf
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-12 22:54:00 +0100

    feat: add styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -89,6 +89,10 @@ const Container = styled.article`
       font-size: 3rem;
     }
 
+    @media only screen and (min-width: 106.25em) {
+      font-size: 4.6rem;
+    }
+
     @media only screen and (min-width: 112.5em) {
       font-size: 4.7rem;
     }
