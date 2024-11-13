a2a13b3cdc32a9d0d0062ad1f9a6e8f164340f43
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-13 11:33:15 +0100

    feat: add styling

--- src/components/hero/DesignHero.tsx
+++ src/components/hero/DesignHero.tsx
@@ -136,6 +136,18 @@ const Text = styled.p`
   @media only screen and (max-width: 18.75em) {
     font-size: var(--default-font-size);
   }
+
+  @media only screen and (min-width: 106.25em) {
+    font-size: 2.6rem;
+  }
+
+  @media only screen and (min-width: 112.5em) {
+    font-size: 2.8rem;
+  }
+
+  @media only screen and (min-width: 125em) {
+    font-size: 3rem;
+  }
 `;
 
 export default DesignHero;
