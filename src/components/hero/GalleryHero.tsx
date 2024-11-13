e1a7c3f0d9cd59ac098a014fb48554eceecc5bea
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-13 11:38:49 +0100

    feat: add media query

--- src/components/hero/GalleryHero.tsx
+++ src/components/hero/GalleryHero.tsx
@@ -101,8 +101,20 @@ const Text = styled.p`
     font-size: 2.2rem;
   }
 
+  @media only screen and (max-width: 26.875em) {
+    font-size: 2.15rem;
+  }
+
   @media only screen and (max-width: 25em) {
     font-size: 2rem;
+  }
+
+  @media only screen and (max-width: 23.4375em) {
+    font-size: 1.85rem;
+  }
+
+  @media only screen and (max-width: 21.5em) {
+    font-size: 1.75rem;
   }
 
   @media only screen and (min-width: 106.25em) {
