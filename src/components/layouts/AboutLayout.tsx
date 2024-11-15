9d0a7d0157d6f393b5725999ef20fde784e39a56
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-15 01:44:04 +0100

    feat: add media query

--- src/components/layouts/AboutLayout.tsx
+++ src/components/layouts/AboutLayout.tsx
@@ -19,59 +19,59 @@ const Container = styled.main`
     [center-end] minmax(6rem, 1fr) [full-end];
 
   @media only screen and (max-width: 64em) {
-    grid-template-rows: 43vw min-content 30vw min-content 23vw;
+    grid-template-rows: 43vw 40vw min-content 20vw min-content;
   }
 
   @media only screen and (max-width: 59.375em) {
-    grid-template-rows: 45vw min-content 30vw min-content 23vw;
+    grid-template-rows: 45vw 40vw min-content 22vw min-content;
   }
 
   @media only screen and (max-width: 56.25em) {
-    grid-template-rows: 47vw min-content 30vw min-content 23vw;
+    grid-template-rows: 47vw 40vw min-content min-content 23vw min-content;
   }
 
   @media only screen and (max-width: 50em) {
-    grid-template-rows: 50vw min-content 30vw min-content 25vw;
+    grid-template-rows: 50vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (max-width: 43.75em) {
-    grid-template-rows: 53vw min-content 30vw min-content 25vw;
+    grid-template-rows: 53vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (max-width: 37.5em) {
-    grid-template-rows: 55vw min-content 30vw min-content 27vw;
+    grid-template-rows: 55vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (max-width: 34.375em) {
-    grid-template-rows: 57vw min-content 30vw min-content 30vw;
+    grid-template-rows: 57vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (max-width: 30em) {
-    grid-template-rows: 60vw min-content 30vw min-content 35vw;
+    grid-template-rows: 60vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (max-width: 26.875em) {
-    grid-template-rows: 65vw min-content 30vw min-content 40vw;
+    grid-template-rows: 65vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (max-width: 25em) {
-    grid-template-rows: 70vw min-content 30vw min-content 43vw;
+    grid-template-rows: 70vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (max-width: 22.5em) {
-    grid-template-rows: 73vw min-content 30vw min-content 43vw;
+    grid-template-rows: 73vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (max-width: 21.5em) {
-    grid-template-rows: 75vw min-content 30vw min-content 45vw;
+    grid-template-rows: 75vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (max-width: 18.75em) {
-    grid-template-rows: 78vw min-content 30vw min-content 45vw;
+    grid-template-rows: 78vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (max-width: 17.5em) {
-    grid-template-rows: 80vw min-content 30vw min-content 45vw;
+    grid-template-rows: 80vw 40vw min-content min-content 24vw min-content;
   }
 
   @media only screen and (min-width: 106.25em) {
