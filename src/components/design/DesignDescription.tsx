d2c868e6156779aa090cbed6ee42cd0cd5917fd3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-16 03:28:31 +0100

    feat: create DesignDescription component

---
+++ src/components/design/DesignDescription.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import Heading from '../Heading';
+
+import Input from '../inputs/Input';
+import TextArea from '../inputs/TextArea';
+
+const DesignDescription = ({ phone, subject, desc, errors, onChange }) => {
+  return (
+    <>
+      <Heading
+        title='Describe your design ideas'
+        subtitle='What inspired the design(s)?'
+      />
+      <Input
+        type='tel'
+        name='phone'
+        label='Phone'
+        value={phone}
+        placeholder='Phone'
+        onChange={onChange}
+        error={errors.phone}
+      />
+      <Input
+        name='subject'
+        label='Subject'
+        value={subject}
+        placeholder='Subject'
+        onChange={onChange}
+        error={errors.subject}
+      />
+      <TextArea
+        name='desc'
+        value={desc}
+        label='Description'
+        placeholder='Description'
+        onChange={onChange}
+        error={errors.desc}
+      />
+    </>
+  );
+};
+
+export default DesignDescription;
