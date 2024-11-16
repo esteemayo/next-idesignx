378031cfd75032e0a53bbace3b7f8f7b4c0eae6e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-16 03:09:59 +0100

    feat: create DesignInfo component

---
+++ src/components/design/DesignInfo.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import Heading from '../Heading';
+import Input from '../inputs/Input';
+
+const DesignInfo = ({ name, email, address, errors, onChange }) => {
+  return (
+    <>
+      <Heading
+        title='Share some basics info about you'
+        subtitle='Tell us about you'
+      />
+      <Input
+        name='name'
+        label='Your name'
+        value={name}
+        placeholder='Your name'
+        onChange={onChange}
+        error={errors.name}
+      />
+      <Input
+        name='email'
+        type='email'
+        label='Your email'
+        value={email}
+        placeholder='Your email'
+        onChange={onChange}
+        error={errors.email}
+      />
+      <Input
+        name='address'
+        label='Your address'
+        value={address}
+        placeholder='Your address'
+        onChange={onChange}
+        error={errors.address}
+      />
+    </>
+  );
+};
+
+export default DesignInfo;
