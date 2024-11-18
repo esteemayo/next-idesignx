e1b94fdcd65aa179fefb41b84e7b71cb43f24c7b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-18 01:17:13 +0100

    feat: add media query

--- src/components/hero/AboutHero.tsx
+++ src/components/hero/AboutHero.tsx
@@ -126,6 +126,10 @@ const Text = styled.p`
     font-size: 1.3rem;
   }
 
+  @media only screen and (max-width: 19.375em) {
+    font-size: 1.25rem;
+  }
+
   @media only screen and (max-width: 18.75em) {
     font-size: 1.2rem;
   }
